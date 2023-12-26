d96a63b742c71a9bd06f883c30bed0264a11ed7a
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2023-12-27 00:02:00 +0900

    Title: 달에서 무게 구하기, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231227/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int weight = 13;
+        float gravityRatio = 0.165f;
+        System.out.printf("%d * %.6f = %.6f",weight,gravityRatio,weight*gravityRatio);
+    }
+}
