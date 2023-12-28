d79b47de3f4867beef945ca0a4cac60e42806b81
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2023-12-28 20:27:16 +0900

    Title: 변수 값 교체하기 5, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231228/변수 값 교체하기 5/replacing-variable-values-5.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 6;
+        int b = a;
+        System.out.println(a);
+        System.out.print(b);
+    }
+}
