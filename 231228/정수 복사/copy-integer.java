3bb1b23dfa9f89f0e04a8aae11afdf9fed1f87be
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2023-12-28 20:28:53 +0900

    Title: 정수 복사, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231228/정수 복사/copy-integer.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a;
+        a = 3;
+        int b;
+        b = 4;
+        b = a;
+        System.out.println(a+" "+b);
+        System.out.print(a*b);
+    }
+}
