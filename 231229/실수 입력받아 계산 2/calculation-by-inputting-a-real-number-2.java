1ca3eed794975fc8b0037207f092ca87c79f1c90
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2023-12-29 19:41:38 +0900

    Title: 실수 입력받아 계산 2, Time: 149ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 231229/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        a=a+1.5D;
+        System.out.printf("%.2f",a);
+
+
+    }
+}
