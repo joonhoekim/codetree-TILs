10fb541c97bc5ce5fc2fa14da7f77615cac0fe52
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2023-12-30 21:59:27 +0900

    Title: 문자 받아 출력, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231230/문자 받아 출력/enter-char-and-print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        char c = s.charAt(0);
+        System.out.println(c);
+    }
+}
