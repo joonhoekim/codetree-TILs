938f64ed4eb16f7aab84c705c2faed6b33727cb9
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-04 00:09:15 +0900

    Title: 입력받아 연산하기, Time: 166ms, Memory: 10MB, Status: Passed - CodeTree

--- 240104/입력받아 연산하기/take-input-and-operate.java
+++ 240104/입력받아 연산하기/take-input-and-operate.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int a = sc.nextInt() + 87;
-        int b = sc.nextInt() / 10;
+        int b = sc.nextInt() % 10;
 
         System.out.printf("%d\n%d",a,b);
 
