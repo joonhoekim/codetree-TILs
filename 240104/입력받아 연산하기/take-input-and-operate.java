236a12393dd24de53d2149f2bdeac41efbc560f7
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-04 00:08:17 +0900

    Title: 입력받아 연산하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240104/입력받아 연산하기/take-input-and-operate.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scnnaer(System.in);
+
+        int a = sc.nextInt() + 87;
+        int b = sc.nextInt() / 10;
+
+        System.out.printf("%d\n%d",a,b);
+
+
+    }
+}
