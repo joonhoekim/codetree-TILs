e0df9f4ac0c009d5f6e154bc609328f7f98b590f
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-06 18:24:02 +0900

    Title: 등급 매기기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240106/등급 매기기/ranking.java
+++ 240106/등급 매기기/ranking.java
@@ -5,7 +5,10 @@ public class Main {
     // Please write your code here.
     Scanner sc = new Scanner(System.in);
     
-    //if - else if - else VS use index... 
+    //if - else if - else VS use index...
+    //제출 확인해보니 if문으로 구성한 것이 120ms 정도로 훨씬 빠르고
+    //내가 작성한건 240ms 정도로 훨씬 느리긴 한데
+    //나중에 확장가능성이 이렇게 다루는게 더 낫겠다...
     int score = sc.nextInt();
     String grade = "ABCDF";
     
