52377ba2e26cf7103e40dd23a391c8e46483b055
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-06 18:20:28 +0900

    Title: 등급 매기기, Time: 172ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240106/등급 매기기/ranking.java
+++ 240106/등급 매기기/ranking.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // Please write your code here.
-        Scanner sc = new Scanner(System.in);
-        int score = sc.nextInt();
-        String grade = "ABCDF";
-        for(int criteria=90, gradeIndex=0; criteria>=60; criteria-=10,gradeIndex++) {
-            if(score>=criteria) {
-                System.out.println(grade.charAt(gradeIndex));
-            }
-            return;
-        }
-        System.out.println(grade.charAt(4));
+  public static void main(String[] args) {
+    // Please write your code here.
+    Scanner sc = new Scanner(System.in);
+    int score = sc.nextInt();
+    String grade = "ABCDF";
+    for(int criteria=90, gradeIndex=0; gradeIndex<grade.length(); criteria-=10,gradeIndex++) {
+      if(score>=criteria) {
+        System.out.println(grade.charAt(gradeIndex));
+        return;
+      }
     }
+    //System.out.println(grade.charAt(4));
+  }
 }
