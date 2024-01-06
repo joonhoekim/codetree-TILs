7742fc42e740ab42913e1615034785d44a828fc7
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-06 15:57:27 +0900

    Title: 시험 통과 여부 확인하기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240106/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        if(n>=80) {
+            System.out.println("pass");
+        } else {
+            System.out.printf("%d more score",80-n);
+        }
+
+               
+    }
+}
