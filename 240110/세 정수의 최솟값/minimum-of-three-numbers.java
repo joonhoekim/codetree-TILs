6d79457982ead2d4feac7be50641b6e5c802d328
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-10 13:58:19 +0900

    Title: 세 정수의 최솟값, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240110/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        for(int i = 0; i<arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min=arr[0];
+        for(int i = 0; i<arr.length-1; i++) {
+            if(arr[i+1]<arr[i]) {
+                min = arr[i+1];
+            }
+        }
+        System.out.print(min);
+
+    }
+}
