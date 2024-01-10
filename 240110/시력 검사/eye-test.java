5c0ed97b01f7c162e83dce8923ff677e0b9a3362
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-10 13:46:29 +0900

    Title: 시력 검사, Time: 158ms, Memory: 11MB, Status: Passed - CodeTree

--- 240110/시력 검사/eye-test.java
+++ 240110/시력 검사/eye-test.java
@@ -9,6 +9,8 @@ public class Main {
 
         if(a >= 1.0 && b >= 1.0) {
             System.out.println("High");
+        } else if (a >= 0.5 && b >= 0.5) {
+            System.out.println("Middle");
         } else{
             System.out.println("Low");
         }
