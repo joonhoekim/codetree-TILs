7f567e5f31396d13c151e901155086eb2a429894
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-11 19:53:31 +0900

    Title: 남녀노소 구분짓기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240111/남녀노소 구분짓기/sex-and-age.java
+++ 240111/남녀노소 구분짓기/sex-and-age.java
@@ -6,17 +6,18 @@ public class Main {
         int sex = sc.nextInt(); //0=M, 1=F
         int age = sc.nextInt();
         if(sex==0){
-            if(age>19) {
+            if(age>=19) {
                 System.out.println("MAN");
             } else {
                 System.out.println("BOY");
             }
         } else {
-            if(age>19) {
+            if(age>=19) {
                 System.out.println("WOMAN");
             } else {
                 System.out.println("GIRL");
             }
+            //코드트리님.. 문제 설명에 이상/이하를 제대로 적어주셔야죠... 영어..
         }
 
 
