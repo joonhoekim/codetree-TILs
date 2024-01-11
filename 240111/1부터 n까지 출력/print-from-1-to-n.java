24516c1de3180b1fe8f1cd490d69a3c3e8e21709
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-11 21:12:44 +0900

    Title: 1부터 n까지 출력, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240111/1부터 n까지 출력/print-from-1-to-n.java
+++ 240111/1부터 n까지 출력/print-from-1-to-n.java
@@ -4,8 +4,8 @@ public class Main {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i = 0; i < n; i++) {
-            System.out.print(i+1+" ");
+        for(int i = 1; i < n+1; i++) {
+            System.out.print(i+" ");
         }
     }
 }
