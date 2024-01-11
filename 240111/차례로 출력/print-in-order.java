7386eed73e6eab2be6dab8b30baf8e662609c8aa
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-11 20:50:58 +0900

    Title: 차례로 출력, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240111/차례로 출력/print-in-order.java
+++ 240111/차례로 출력/print-in-order.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i=5, i<=18; i++) {
+        for(int i=5; i<=17; i++) {
             System.out.println(i);
         }
     }
