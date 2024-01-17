a26a330bab09ada251e1fba85aab688d52c4de59
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-17 21:48:09 +0900

    Title: 문자 출력하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240117/문자 출력하기/print-text.java
+++ 240117/문자 출력하기/print-text.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+        char input = sc.nextLine().charAt(0);
         for(int i = 0; i<8; i++) {
             System.out.print(input);
         }
