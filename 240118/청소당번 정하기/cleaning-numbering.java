043fa43c4cb4c2c436d9e94fa53c46e7ad8ac8d8
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-18 20:56:14 +0900

    Title: 청소당번 정하기, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240118/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int given = sc.nextInt();
+    int classroom = 0; //per 2days
+    int corridor = 0; //per 3days
+    int bathroom = -1; //per 12days, when we use residue for check, day0 is always bathroom cleaning day.
+
+    for (int i = 0; i <= given; i++) {
+      if (i % 12 == 0) {
+        bathroom++;
+      } else if (i % 3 == 0) {
+        corridor++;
+      } else if (i % 2 == 0) {
+        classroom++;
+      }
+    }
+
+    System.out.printf("%d %d %d", classroom, corridor, bathroom);
+
+
+  }
+}
