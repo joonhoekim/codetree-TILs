7612ef93cb74b42d23fd86dbde0b8b5521995f94
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-18 20:37:45 +0900

    Title: 369 게임, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240118/369 게임/369-game.java
+++ 240118/369 게임/369-game.java
@@ -5,8 +5,11 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int given = sc.nextInt();
+    //charAt 쓰는게 더 쉬워보이긴 하는데
+    //오기 생겨서 String 클래스 안써보고 풀어봅니다.
+
     for (int i = 1; i <= given; i++) {
-      //multiple of 3
+      int tensPlace = i / 10;
       if (i % 3 == 0) {
         System.out.print("0 ");
       }
@@ -18,11 +21,11 @@ public class Main {
       } else if ((i + 1) % 10 == 0) {
         System.out.print("0 ");
       } //contains 3, 6, 9 (10의 자리수)
-      else if ((i - 30) / 10 == 0) {
+      else if ((tensPlace + 7) % 10 == 0) {
         System.out.print("0 ");
-      } else if ((i - 60) / 10 == 0) {
+      } else if ((tensPlace + 4) % 10 == 0) {
         System.out.print("0 ");
-      } else if ((i - 90) / 10 == 0) {
+      } else if ((tensPlace + 1) % 10 == 0) {
         System.out.print("0 ");
       }
       //그 외
