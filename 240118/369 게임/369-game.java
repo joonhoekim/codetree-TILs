e0af30b512e5080e2fb39b3f08ca47a520941672
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-18 20:25:31 +0900

    Title: 369 게임, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240118/369 게임/369-game.java
+++ 240118/369 게임/369-game.java
@@ -10,10 +10,23 @@ public class Main {
       if (i % 3 == 0) {
         System.out.print("0 ");
       }
-      //contains 3
+      //contains 3, 6, 9 (일의 자리수)
       else if ((i + 7) % 10 == 0) {
         System.out.print("0 ");
-      } else {
+      } else if ((i + 4) % 10 == 0) {
+        System.out.print("0 ");
+      } else if ((i + 1) % 10 == 0) {
+        System.out.print("0 ");
+      } //contains 3, 6, 9 (10의 자리수)
+      else if ((i - 30) / 10 == 0) {
+        System.out.print("0 ");
+      } else if ((i - 60) / 10 == 0) {
+        System.out.print("0 ");
+      } else if ((i - 90) / 10 == 0) {
+        System.out.print("0 ");
+      }
+      //그 외
+      else {
         System.out.print(i + " ");
       }
 
