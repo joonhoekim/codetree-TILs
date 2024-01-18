d7988dec719a862c8a8c718853660a384e6dc9ba
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-18 20:14:06 +0900

    Title: 높은 수에서 낮은 수까지, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240118/높은 수에서 낮은 수까지/from-high-to-low.java
+++ 240118/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -5,8 +5,18 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt(), b = sc.nextInt();
-    while(a<=b){
+    if (b>a) {
+        while(a<=b){
         System.out.print(b--+" ");
+        }
+    } else {
+        int temp = a;
+        a = b;
+        b = temp;
+        while(a<=b){
+        System.out.print(b--+" ");
+        }
     }
+    
   }
 }
