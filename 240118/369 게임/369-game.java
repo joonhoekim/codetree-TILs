b893966d1cad79fefa007a268902e1115ac15d6b
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-18 20:47:21 +0900

    Title: 369 게임, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240118/369 게임/369-game.java
+++ 240118/369 게임/369-game.java
@@ -5,18 +5,22 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int given = sc.nextInt();
+
+    long startTime = System.currentTimeMillis();
+
     for (int i = 1; i <= given; i++) {
-      String iToString = String.valueOf(i);
 
       if (i % 3 == 0) {
         System.out.print("0 ");
-      } else if (iToString.contains("3")
-          || iToString.contains("6")
-          || iToString.contains("9")) {
+      } else if (String.valueOf(i).contains("3")
+          || String.valueOf(i).contains("6")
+          || String.valueOf(i).contains("9")) {
         System.out.print("0 ");
       } else {
         System.out.print(i + " ");
       }
     }
+
+    System.out.println("\n" + (System.currentTimeMillis() - startTime));
   }
 }
