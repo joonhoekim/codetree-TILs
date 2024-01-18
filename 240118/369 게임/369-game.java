30c1d31e1cd3a34e0176ef9d349b6f3f3068c9ba
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-18 20:49:08 +0900

    Title: 369 게임, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240118/369 게임/369-game.java
+++ 240118/369 게임/369-game.java
@@ -4,19 +4,21 @@ public class Main {
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    StringBuilder sb = new StringBuilder();
     int given = sc.nextInt();
 
     for (int i = 1; i <= given; i++) {
 
       if (i % 3 == 0) {
-        System.out.print("0 ");
+        sb.append("0 ");
       } else if (String.valueOf(i).contains("3")
           || String.valueOf(i).contains("6")
           || String.valueOf(i).contains("9")) {
-        System.out.print("0 ");
+        sb.append("0 ");
       } else {
-        System.out.print(i + " ");
+        sb.append(i + " ");
       }
     }
+    System.out.print(sb);
   }
 }
