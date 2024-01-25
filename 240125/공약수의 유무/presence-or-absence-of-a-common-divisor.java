9bc01baf46fa7c6f93c7bde0ca5293f712709f9c
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-25 21:43:21 +0900

    Title: 공약수의 유무, Time: 203ms, Memory: 10MB, Status: Passed - CodeTree

--- 240125/공약수의 유무/presence-or-absence-of-a-common-divisor.java
+++ 240125/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -6,8 +6,8 @@ public class Main {
         int a = sc.nextInt(), b =sc.nextInt();
         boolean satisfied = false;
         //a<=b 라는 조건이 있으므로 비교없이 바로
-        for(int i=2; i<=a; i++) {
-            if (a%i==0 && b%i==0) {
+        for(int i=a; i<=b; i++) {
+            if (1920%i==0 && 2880%i==0) {
                 satisfied=true;
                 break;
             }
