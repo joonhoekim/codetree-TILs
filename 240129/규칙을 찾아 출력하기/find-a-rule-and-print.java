f3533ffdebedce1b31633744c2ba86225b3d8fbe
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-29 19:55:45 +0900

    Title: 규칙을 찾아 출력하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240129/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=n;i>=1;i--){ //이렇게 풀면 4사분면이 아니라 1사분면 모양이 된다.
+            for(int j=n;j>=1;j--){
+                if(i==1 || j==1 || i==n || j==n) {
+                    System.out.print("* ");
+                } else if(j>i){
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.print("\n");
+        }
+        
+    }
+}
