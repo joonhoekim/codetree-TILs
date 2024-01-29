a948b94b02cbc7d4ce386b781ebfdc6696a8f183
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-29 17:24:25 +0900

    Title: 별 그리기, Time: 213ms, Memory: 12MB, Status: Wrong Answer - CodeTree

---
+++ 240129/별 그리기/star-drawing.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<2*n-1;i++){
+            int blankCnt;
+            int starCnt;
+            if(i<n) {
+                blankCnt = n-i-1;
+                starCnt = 2*i +1;
+            } else {
+                blankCnt = i-n+1;
+                starCnt = 2*n-2*i+3;
+            }
+
+            for(int j=0;j<blankCnt;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<starCnt;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
