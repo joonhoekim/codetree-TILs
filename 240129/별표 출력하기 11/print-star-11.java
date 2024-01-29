c654eaffa1d404a2f1feb3c62cc8780712d27c45
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-29 19:38:31 +0900

    Title: 별표 출력하기 11, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240129/별표 출력하기 11/print-star-11.java
+++ 240129/별표 출력하기 11/print-star-11.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        n=n*2+1;
+        count=n*2+1;
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
+        for(int i=1;i<=count;i++){
+            for(int j=1;j<=count;j++){
                 if(i%2==0 && j%2==0) {
                     System.out.print("  ");
                 } else {
