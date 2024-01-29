adcff588825dd9981151a8b47504c7146b89372d
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-29 16:23:34 +0900

    Title: 별표 출력하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240129/별표 출력하기/print-star.java
+++ 240129/별표 출력하기/print-star.java
@@ -4,20 +4,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        for (int i=0; i<n; i++){
-            for(int j=0; j<=i; j++){
+        int cnt = 0;
+        for (int i=0; i<2*n-1; i++){
+            if (i>=n) {
+                cnt--;
+            } else {
+                cnt++;
+            }
+            for(int j=0; j<cnt; j++){
                 System.out.print("* ");
             }
             System.out.print("\n");
         }
-        for (int i=0; i<n; i++){
-            for(int j=n; j>i+1; j--){
-                System.out.print("* ");
-            }
-            System.out.print("\n");
-        }
-        
-
     }
 }
+
+//case1
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+
+        // for (int i=0; i<n; i++){
+        //     for(int j=0; j<=i; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.print("\n");
+        // }
+        // for (int i=0; i<n; i++){
+        //     for(int j=n; j>i+1; j--){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.print("\n");
