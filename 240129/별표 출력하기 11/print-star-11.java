b126c5cf0aeec4f550005db6479398daa83c402a
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-29 19:38:56 +0900

    Title: 별표 출력하기 11, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240129/별표 출력하기 11/print-star-11.java
+++ 240129/별표 출력하기 11/print-star-11.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        count=n*2+1;
+        int count=n*2+1;
 
         for(int i=1;i<=count;i++){
             for(int j=1;j<=count;j++){
