b1be90a9a2eda8e47d3fae4e90795991a7c999ed
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-30 18:39:00 +0900

    Title: 구구단 만들기, Time: 139ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240130/구구단 만들기/print-multiplication-table.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        for(int i=1; i<=9;i++){
+            for(int j=b; j>=a; j--){
+                if(j%2==0){
+                    System.out.printf("%d * %d = %d",j,i,j*i);
+                    if(j>2) {
+                        System.out.print(" / ");
+                    }
+                }
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
