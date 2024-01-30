986ac7c28a26ae8115c4a4d63437e7e68c01d297
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-30 16:17:30 +0900

    Title: 9-1 정사각형, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240130/9-1 정사각형/9-1-square.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 9;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n; j++){
+                System.out.printf("%d", count--);
+                if(count==0){
+                    count=9;
+                }
+            }
+            System.out.print("\n");
+        }       
+    }
+}
