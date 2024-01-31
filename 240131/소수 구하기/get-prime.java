20fba17764990cd2eed1d3569c1d965c4e67274e
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-31 18:36:16 +0900

    Title: 소수 구하기, Time: 232ms, Memory: 10MB, Status: Passed - CodeTree

--- 240131/소수 구하기/get-prime.java
+++ 240131/소수 구하기/get-prime.java
@@ -9,8 +9,10 @@ public class Main {
         for (int i=2; i<=n; i++) {
             boolean isPrime = true;
             for( int j=2; j<i; j++) {
-                if(i%j==0) isPrime = false;
-                break;
+                if(i%j==0) {
+                    isPrime = false;
+                    break;
+                }
             }
             if(isPrime) {
                 sb.append(i+" ");
