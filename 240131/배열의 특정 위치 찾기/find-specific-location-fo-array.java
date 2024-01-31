ba9f8e719a49edc2d4fc2a2c64328616e4851b3a
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-31 20:40:50 +0900

    Title: 배열의 특정 위치 찾기, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

--- 240131/배열의 특정 위치 찾기/find-specific-location-fo-array.java
+++ 240131/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -4,27 +4,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = 10; // sc.nextInt();
-
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++){
-            arr[i]=sc.nextInt();
-        }
-
         int sumOfEven=0;
-        for(int i=1; i<n; i+=2){
-            sumOfEven+=arr[i];
-        }
-
         int sumOfThree=0;
         int countOfThree=0;
+        int[] arr = new int[n];
 
-        for(int i=2; i<n; i+=3) {
-            sumOfThree += arr[i];
-            countOfThree++;
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+            if((i+1)%2==0) {
+                sumOfEven+=arr[i];
+            }
+
+            if((i+1)%3==0) {
+                sumOfThree += arr[i];
+                countOfThree++;
+            }
         }
+
         double avgOfThree = (double) sumOfThree/countOfThree;
-
-
         System.out.printf("%d %.1f",sumOfEven,avgOfThree);
 
     }
