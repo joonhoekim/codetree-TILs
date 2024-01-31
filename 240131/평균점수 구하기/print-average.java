1eb0ebca22ada3e47cf49b29b332d4fbe59c99d9
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-01-31 19:35:58 +0900

    Title: 평균점수 구하기, Time: 165ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240131/평균점수 구하기/print-average.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 8;
+
+        double sum=0.0;
+
+        for(int i=0; i<8; i++) {
+            sum+=sc.nextDouble();
+        }
+
+        System.out.printf("%.1f",sum/n);
+    }
+}
