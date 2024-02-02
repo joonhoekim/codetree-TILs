d4dfda6fc551b1a2015c0b495941fcad05e44d5d
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-02 19:02:54 +0900

    Title: 숫자들의 배수, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240202/숫자들의 배수/multiple-of-numbers.java
+++ 240202/숫자들의 배수/multiple-of-numbers.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int multiplier = 1;
-        int cnt5 = 0;
-        for(;;){
-            int temp=n*multiplier++;
-            System.out.print(temp+" ");
-
-            if(temp%5==0) {
-                cnt5++;
-                if(cnt5==2){
+        int[] arr = new int [10];
+        int count=0;
+        //*5, *10 에서는 모든 경우에서 5의 배수이므로 최대 10번안에 끝난다.
+        for(int i=0;i<10;i++){
+            arr[i]=n*(i+1);
+            System.out.print(arr[i]+" ");
+            if(arr[i]%5==0){
+                count++;
+                if(count==2){
                     break;
                 }
             }
