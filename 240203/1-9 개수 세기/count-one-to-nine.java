8fc710b549055fe0174c2d7c509cbdd467e1418a
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-03 15:54:08 +0900

    Title: 1-9 개수 세기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240203/1-9 개수 세기/count-one-to-nine.java
+++ 240203/1-9 개수 세기/count-one-to-nine.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arrCount = new int[9];
+        int[] arrCount = new int[10];
         for(int i=0; i<n; i++){
             int input = sc.nextInt();
-            arrCount[input-1]++;            
+            arrCount[input]++;            
         }
-        for(int i=0;i<9;i++){
+        for(int i=1;i<10;i++){
             System.out.println(arrCount[i]);
         }
     }
