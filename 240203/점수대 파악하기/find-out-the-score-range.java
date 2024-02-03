40d820b7460ad9c23dd812d968d323cd73f9743b
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-03 16:12:57 +0900

    Title: 점수대 파악하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240203/점수대 파악하기/find-out-the-score-range.java
+++ 240203/점수대 파악하기/find-out-the-score-range.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] score_10 = new int[10];
-        for(;;){
-            int input = sc.nextInt();
-            if (input == 0){
-                break;
-            }
-            score_10[input/10 -1]++;
-        }
 
-        for(int i=0; i<10; i++) {
-            System.out.printf("%d - %d\n",(10-i)*10, score_10[9-i]);
-        }
-        
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int[] score_10 = new int[10];
+    for (; ; ) {
+      int input = sc.nextInt();
+      if (input == 0) {
+        break;
+      } else if (input < 10) {
+        continue;
+      }
+      score_10[input / 10 - 1]++;
     }
+
+    for (int i = 0; i < 10; i++) {
+      System.out.printf("%d - %d\n", (10 - i) * 10, score_10[9 - i]);
+    }
+
+  }
 }
