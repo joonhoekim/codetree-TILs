984863e74beb47a4441129758607b83db2b997ce
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-05 21:43:49 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 232ms, Memory: 13MB, Status: Passed - CodeTree

--- 240205/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 240205/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -1,26 +1,34 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        //배열 1000개 만들어서 쓰는게 구현이 쉽다. 
+        //메모리를 N개만큼만 쓰는 것도 구현해보자.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        int maxVal = 1001;
-        int[] count = new int[maxVal]; //이게 좋은 건 아닌 것 같지만..
-        for(int i=0; i<n;i++){
+       
+        for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
-            count[arr[i]]++;
         }
 
-        for(int i=maxVal-1;i>=0;i--) {
-            if(count[i]==1){
-                System.out.print(i);
-                return;
+        int max = -1;
+        for(int i=0;i<n;i++){
+            if(arr[i]>max) {
+                int count=0;
+                for(int j=0;j<n;j++){
+                    if(arr[j]==arr[i]) {
+                        count++;
+                    }
+                }
+                if(count==1){
+                    max=arr[i];
+                }
             }
         }
 
-        System.out.print(-1);
+        System.out.print(max);
 
 
-        
+
     }
 }
