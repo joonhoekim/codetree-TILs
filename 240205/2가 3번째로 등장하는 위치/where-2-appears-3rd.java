8918d0fe43c50f431cffcd6242296e0b41ea55ed
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-05 19:34:55 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 244ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240205/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int count = 0;
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]==2) {
+                count++;
+                if(count==3){
+                    System.out.print(i+1);
+                    return;
+                }
+            }
+        }
+    }
+}
