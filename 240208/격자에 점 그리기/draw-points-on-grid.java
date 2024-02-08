ee490f22bf6c49adcf496b448c7b3afca52c9edc
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-08 16:51:22 +0900

    Title: 격자에 점 그리기, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240208/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //격자크기
+        int m = sc.nextInt(); //동전개수
+        int count = 1;
+
+        int[][] brd = new int[n][n];
+        
+        for(int i=0; i<m; i++){
+            int rowIdx = sc.nextInt() - 1 ;
+            int colIdx = sc.nextInt() - 1 ;
+
+            brd[rowIdx][colIdx]=count++;
+        }
+
+        for(int rowIdx = 0; rowIdx<n; rowIdx++){
+            for(int colIdx = 0; colIdx<n; colIdx++) {
+                System.out.print(brd[rowIdx][colIdx]+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
