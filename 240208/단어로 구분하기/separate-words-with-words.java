5e5b8609b613d5ea72277674c076d66d80c7c6ea
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-08 21:34:20 +0900

    Title: 단어로 구분하기, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240208/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 10;
+        String[] arr = new String[n];
+        for (int i=0; i<n; i++){
+            arr[i] = sc.next();
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.println(arr[i]);
+        }
+        
+    }
+}
