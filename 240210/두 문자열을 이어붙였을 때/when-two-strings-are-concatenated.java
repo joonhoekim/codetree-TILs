9f09a70a9b0b100a51839f31d1dc4c5da7825107
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-10 15:23:09 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 130ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240210/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.java
+++ 240210/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.java
@@ -10,12 +10,23 @@ public class Main {
         String t1 = str1+str2;
         String t2 = str2+str1;
 
-        if(t1.equals(t2)){
+        // if(t1.equals(t2)){
+        //     System.out.print("true");
+        // } else{
+        //     System.out.print("false");
+        // }
+
+        boolean b = false;
+        for(int i=0;i<t1.length();i++){
+            if(t1.charAt(i)==t2.charAt(i)){
+                b=true;
+            }
+        }
+
+        if(b){
             System.out.print("true");
         } else{
             System.out.print("false");
         }
-
-        
     }
 }
