f81e14b1b5018296a229bfcd552acfbcbde2ad7c
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-11 18:44:26 +0900

    Title: 그 다음 알파벳, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240211/그 다음 알파벳/next-alphabet.java
+++ 240211/그 다음 알파벳/next-alphabet.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char a = (char) ( (int)sc.next().charAt(0) + 1 );
+        if(a - 1 == 'z') {
+            a = 'a';
+        }
         //int b = (int) sc.next().charAt(0);
         System.out.printf("%c",a);
 
