371f837dc57241c47decbab89aa3f6e584defb42
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-11 16:28:13 +0900

    Title: 문자 제거하기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240211/문자 제거하기/del-char.java
+++ 240211/문자 제거하기/del-char.java
@@ -3,27 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[] cArr = sc.nextLine().toCharArray();
-        int[] removed = new int[cArr.length];
+        String str = sc.nextLine();
+
+        //이 문제는, 제거한 문자열을 기준으로 인덱스를 적용하는 것인지
+        //제거하기 전 문자열의 인덱스를 기준으로 제거한다는 것인지
+        //그게 명확하지가 않아서 예제를 보고 판단해야하네...
+        //예제 보면 '제거한 후 문자열'이 기준이 된다.
+
+        //만약 최초 문자열을 계속 기준으로 한다면
+        //인덱스를 나타내는 배열을 만들어서 푸는 게 좋다.
         
-        int size = cArr.length;
-        while(size>1){
-            size=0;
+        int size = str.length();
+        do {
             int n = sc.nextInt();
-            if(n>=cArr.length) {
-                n=cArr.length-1;
+            if(n >= str.length()){
+                str = str.substring(0,str.length()-1);
+                size--;
+                System.out.println(str);
+                continue;
+            } else {
+                str = str.substring(0,n) + str.substring(n+1,str.length());
+                size--;
+                System.out.println(str);
             }
-            removed[n]++;
-            
-            for(int i=0; i<cArr.length; i++){
-                if (removed[i]==0){
-                    System.out.print(cArr[i]);
-                    size++;
-                }
-            }
-            System.out.println("");
-
-
-        }
+        } while (size > 1);
+        
     }
 }
