d6dcb1762599a2103fce96463db07284ff7e99ad
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-11 19:24:40 +0900

    Title: 대문자 소문자 바꾸기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240211/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] arr= sc.nextLine().toCharArray();
+        int len = arr.length;
+        int sum = 0;
+
+        for(int i=0;i<len;i++){
+            char target = arr[i];
+            
+            if('A' <= target && target <= 'Z') {
+                 System.out.printf("%c",target+32);
+            } else if('a' <= target && target <= 'z'){
+                 System.out.printf("%c",target-32);
+            }
+            // } if('0' <= target && target <= '9') {
+            //     //sum+=Integer.valueOf(target-'0');
+            // } else {
+            //     continue;
+            // }
+        }
+
+        // System.out.print(sum);
+
+    }
+}
