c12952ec2d6a12dd75553d972f3e23b0aa54f43d
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-11 15:12:16 +0900

    Title: 첫 번째와 두 번째 교환, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240211/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
+++ 240211/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
@@ -1,45 +1,30 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        char[] cArr = str.toCharArray();
+    public static void main(String[] args) throws IOException {
 
-        char first = cArr[0];
-        int[] firstCheck = new int[cArr.length];
-        int firstSize = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // StringTokenizer st = new StringTokenizer(br.readLine());
 
-        char second = cArr[1];
-        int[] secondCheck = new int[cArr.length];
-        int secondSize = 0;
+        char[] charArray = br.readLine().toCharArray();
 
+        char first = charArray[0];
+        char second = charArray[1];
 
-        //첫번째 글짜와 같은 경우를 firstCheck에 인덱스들로 저장하기
-        for(int i=0; i<cArr.length;i++){
-            if(cArr[0]==cArr[i]){
-                firstCheck[firstSize++]=i;
+        for (int i = 0; i < charArray.length; i++) {
+            if (first == charArray[i]) {
+                charArray[i] = second;
+            } else if (second == charArray[i]) {
+                charArray[i] = first;
             }
         }
 
-        //두번째 글짜와 같은 경우를 secondCheck에 인덱스들로 저장하기
-        for(int i=0; i<cArr.length;i++){
-            if(cArr[1]==cArr[i]){
-                secondCheck[secondSize++]=i;
-            }
-        }
+        bw.write(String.valueOf(charArray));
 
-        for(int i=0;i<firstSize;i++){
-            cArr[firstCheck[i]]=second;
-        }
-
-        for(int i=0;i<secondSize;i++){
-            cArr[secondCheck[i]]=first;
-        }
-
-        System.out.print(cArr);
-
-
-
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
