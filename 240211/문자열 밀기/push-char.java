5bd1c47689f84a672f24fadf694584f6769eff45
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-11 16:42:28 +0900

    Title: 문자열 밀기, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240211/문자열 밀기/push-char.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        str = str.substring(1,str.length()) + str.substring(0,1);
+        System.out.print(str);
+    }
+}
