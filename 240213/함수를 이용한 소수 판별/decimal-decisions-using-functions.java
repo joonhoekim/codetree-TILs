bc09479c57cbc7cd62478c42b7fb3b26a835a6a9
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-13 20:28:29 +0900

    Title: 함수를 이용한 소수 판별, Time: 184ms, Memory: 10MB, Status: Passed - CodeTree

--- 240213/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240213/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -13,7 +13,11 @@ public class Main {
     }
 
     static boolean isPrime(int n){
+        if (n==1) {
+            return false;
+        }
         for(int i=2;i<n;i++){
+            
             if(n%i==0){
                 return false;
             }
