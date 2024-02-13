4bcf2c8bffe4a051716a5f3057b202640538686b
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-13 20:24:29 +0900

    Title: 함수를 이용한 소수 판별, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240213/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+        for(int i=a;i<=b;i++) {
+            if (isPrime(i)) {
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+    }
+
+    static boolean isPrime(int n){
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
