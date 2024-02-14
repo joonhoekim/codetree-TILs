549e26e3fa2f7b44d1c01f90c7945600955bf707
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-14 20:46:43 +0900

    Title: 사칙연산 함수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240214/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] query = sc.nextLine().split(" ");
+        int a = Integer.parseInt(query[0]);
+        char o = query[1].charAt(0);
+        int c = Integer.parseInt(query[2]);
+
+        System.out.printf("%d %c %d = %d",a,o,c,operation(a,o,c));
+    }
+
+    static int operation(int a, char o, int c) {
+        switch(o) {
+            case '+': return a+c;
+            case '-': return a-c;
+            case '*': return a*c;
+            case '/': return a/c;
+        }
+        System.out.println("False");
+        System.exit(-1);
+        return -1;
+    }
+}
