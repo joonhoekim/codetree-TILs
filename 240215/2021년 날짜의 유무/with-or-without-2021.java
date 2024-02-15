0e8e2bb4f12c362666fe793def057884034ede44
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-15 19:25:25 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240215/2021년 날짜의 유무/with-or-without-2021.java
+++ 240215/2021년 날짜의 유무/with-or-without-2021.java
@@ -13,32 +13,11 @@ public class Main {
     }
 
     static boolean valMD(int M,int D){
-        if(12<M) {
+        int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
+        if(maxDays[M-1]<D) {
             return false;
-        }
-        if(31<D) {
-            return false;
-        }
-
-        if(M==2) {
-            if(1<=D && D<=28) {
-                return true;
-            } else {
-                return false;
-            }
-        } else if(M==1 || M==3 || M==5 || M==7 || M==8 || M==10 || M==12) {
-            if(1<=D && D<=31) { //문제조건상1이상은 보장되므로 사실 안해도 됨.
-                return true;
-            } else {
-                return false;
-            }
-
         } else {
-            if(1<=D && D<=30) { //문제조건상1이상은 보장되므로 사실 안해도 됨.
-                return true;
-            } else {
-                return false;
-            }
+            return true;
         }
     }
 }
