84b44a0b8b715693546df9adbf9e55d2feb24d99
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-15 19:01:36 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240215/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int[] arr1 = get1dArr(n1, sc);
+        int[] arr2 = get1dArr(n2, sc);
+        if(isSubsequance(arr2,arr1)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    static int[] get1dArr(int n, Scanner sc) {
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    static boolean isSubsequance(int[] subArr, int[] superArr) {
+        for(int i=0; i<=superArr.length - subArr.length; i++) {
+            boolean isSub = true;
+            for(int j=i; j<i+subArr.length; j++) {
+                if(superArr[i+j] != subArr[j]){
+                    isSub=false;
+                    break;
+                }
+                if(isSub) return true;
+            }
+        }
+        return false;
+    }
+}
