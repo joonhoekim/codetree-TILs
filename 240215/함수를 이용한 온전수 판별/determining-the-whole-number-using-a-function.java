0458d7db60114daeef273e0a4d2ddd5915c86d38
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-15 18:25:50 +0900

    Title: 함수를 이용한 온전수 판별, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240215/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,28 @@
+import java.util.Scanner; 
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.print(countComplete(a,b));
+    }
+
+    static int countComplete(int a, int b) {
+        int count = 0;
+        for(int i=a;i<=b;i++){
+            if(i%2==0) {
+                continue;
+            }
+            if(i%10==5) {
+                continue;
+            }
+            if(i%3==0 && i%9!=0){
+                continue;
+            }
+            
+            count++;
+        }
+        return count;
+    }
+}
