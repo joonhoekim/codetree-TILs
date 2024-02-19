761e53841d3eafe28c2aeed6f061486a72ef8e29
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-19 20:55:33 +0900

    Title: 가장 긴 증가하는 부분 수열 2, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240219/가장 긴 증가하는 부분 수열 2/longest-increasing-subsequence-2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        int[] lenArr = new int[n];
+        for(int i=0;i<n;i++) {
+
+            int cnt=1;
+            for(int j=i; j<n-1; j++) {
+                if(arr[j] < arr[j+1]) {
+                    cnt++;
+                } else {
+                    break;
+                }
+            }
+            lenArr[i] = cnt;
+        }
+        Arrays.sort(lenArr);
+        System.out.print(lenArr[n-1]);
+    }
+}
