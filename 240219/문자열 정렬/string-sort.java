b07fa71f20a6985e31e6ae2c669582147d6c0517
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-19 20:43:23 +0900

    Title: 문자열 정렬, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240219/문자열 정렬/string-sort.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        str = new String(arr);
+        System.out.print(str);
+    }
+}
