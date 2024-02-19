a711fe1ba8a3803d1d463d5c220d5c7b6cbf18b0
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-19 21:51:40 +0900

    Title: 가장 긴 증가하는 부분 수열 2, Time: 3071ms, Memory: 22MB, Status: Time Limit Exceed - CodeTree

--- 240219/가장 긴 증가하는 부분 수열 2/longest-increasing-subsequence-2.java
+++ 240219/가장 긴 증가하는 부분 수열 2/longest-increasing-subsequence-2.java
@@ -1,13 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n =sc.nextInt();
+  public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
+        String[] input = br.readLine().split(" ");
         for(int i=0; i<n; i++) {
-            arr[i]=sc.nextInt();
+            arr[i]=Integer.parseInt(input[i]);
         }
 
 
