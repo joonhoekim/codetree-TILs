9b2a5708dbff33bbc8307863e4f55ff1598df9fe
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-20 19:59:09 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 221ms, Memory: 19MB, Status: Passed - CodeTree

--- 240220/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
+++ 240220/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.java
@@ -1,37 +1,30 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        char[] str1 = br.readLine().toCharArray();
-        char[] str2 = br.readLine().toCharArray();
-        int len1 = str1.length;
-        int len2 = str2.length;
-        if(len1 != len2) {
-            bw.write("No");
-            bw.flush();
-            bw.close();
-            br.close();
-            return;
-        }
-        Arrays.sort(str1);
-        Arrays.sort(str2);
-        for(int i=0;i<str1.length;i++){
-            if(str1[i] != str2[i]) {
-                bw.write("No");
-                bw.flush();
-                bw.close();
-                br.close();
-                return;
-            }
-        }
 
-        bw.write("Yes");
-        bw.flush();
-        bw.close();
-        br.close();
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    char[] str1 = br.readLine().toCharArray();
+    char[] str2 = br.readLine().toCharArray();
+    Arrays.sort(str1);
+    Arrays.sort(str2);
+
+    if (Arrays.toString(str1).equals(Arrays.toString(str2))) {
+      bw.write("Yes");
+    } else {
+      bw.write("No");
     }
+
+    bw.flush();
+    bw.close();
+    br.close();
+    return;
+
+  }
 }
