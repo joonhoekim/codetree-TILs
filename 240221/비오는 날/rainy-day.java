9480fc99f44cec3b11245d75b674c7e0da115967
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-21 01:28:22 +0900

    Title: 비오는 날, Time: 210ms, Memory: 10MB, Status: Passed - CodeTree

--- 240221/비오는 날/rainy-day.java
+++ 240221/비오는 날/rainy-day.java
@@ -18,27 +18,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        WeatherData[] wds = new WeatherData[n];
-        for(int i=0;i<n;i++) {
-            wds[i] = new WeatherData(sc.next(), sc.next(), sc.next());
-        }
+        //WeatherData[] wds = new WeatherData[n];
 
+        WeatherData ans = new WeatherData("9999-99-99","","");
         String condi = "Rain";
-        int tgtIdx = -1;
+
         for(int i=0;i<n;i++) {
-            if(wds[i].weather.equals(condi)){
-                tgtIdx = i;
-                break;
+            WeatherData w = new WeatherData(sc.next(), sc.next(), sc.next());
+            if(w.weather.equals(condi)) {
+                if(w.date.compareTo(ans.date) < 0) {
+                    ans = w;
+                }
             }
         }
 
-        for(int i=0;i<n;i++) {
-            if(wds[tgtIdx].date.compareTo(wds[i].date)>0 && wds[i].weather.equals(condi)) {
-                tgtIdx = i;
-            }
-        }
-        
-        wds[tgtIdx].print();
+        ans.print();
 
     }
 }
