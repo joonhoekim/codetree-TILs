0c3ff4e35d4770d5e5bb3af4f657297823ab2f93
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-22 19:52:24 +0900

    Title: 원점으로부터의 거리, Time: 250ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240222/원점으로부터의 거리/distance-from-origin.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+class Coord implements Comparator<Coord> {
+    int x;
+    int y;
+    int idx;
+
+    Coord(int x, int y, int idx) {
+        this.x=x;
+        this.y=y;
+        this.idx = idx;
+    }
+
+    public void print() {
+        System.out.println(idx);
+    }
+
+    @Override
+    public int compare(Coord a, Coord b) {
+        return ((a.x+ a.y) - (b.x+b.y));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        Coord[] coords = new Coord[n];
+
+        for(int i=0;i<n;i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            coords[i] = new Coord(x,y,i+1);
+        }
+
+        Arrays.sort(coords, (a,b) -> {
+            return ((a.x+ a.y) - (b.x+b.y)); 
+        });
+
+        for(Coord coord : coords) {
+            coord.print();
+        }
+    }
+}
