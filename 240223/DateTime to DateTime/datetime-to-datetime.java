1981cc33b118996bdfd1dd8150d0479d0ad7bc8a
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-23 21:38:37 +0900

    Title: DateTime to DateTime, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240223/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    final static int[] numOfDays = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; //no reap year
+
+    static long dateToMin(int day, int hour, int minute) { //월 계산이 없어서 간단해지네...
+        return day*24*60 + hour*60 + minute;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int day = sc.nextInt();
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+
+        long diff = dateToMin(day,hour,minute) - dateToMin(11,11,11);
+        System.out.print(diff);
+    }
+}
