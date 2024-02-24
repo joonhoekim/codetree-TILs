7984354aabbb354b28fe85ab83454eca7aa41665
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-24 21:01:37 +0900

    Title: 10진수로 변환하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240224/10진수로 변환하기/convert-to-decimal.java
+++ 240224/10진수로 변환하기/convert-to-decimal.java
@@ -3,18 +3,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String inputBin = sc.next();
-        int len = inputBin.length();
-        int[] inputArr = new int[len];
-        for(int i=0; i<len; i++) {
-            inputArr[i] = inputBin.charAt(i) - '0';
+        String bin = sc.next();
+
+        int base10 = 0;
+        for(int i=0; i<bin.length(); i++) {
+            base10 = base10*2 + (bin.charAt(i)-'0');
         }
 
-        int n=0;
-        for(int i=0; i<len; i++) {
-            n = n*2 + inputArr[i];
-        }
-
-        System.out.println(n);
+        System.out.println(base10);
     }
 }
