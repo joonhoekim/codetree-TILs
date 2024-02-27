9fdb67a0ee83078f9107f0ca74770a1a00748a5c
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-27 10:31:08 +0900

    Title: 사각형의 총 넓이 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240227/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
+++ 240227/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -7,7 +7,7 @@ public class Main {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int boardSize = 100+offset+1;
+        int boardSize = 100+offset+2;
         int[][] board = new int[boardSize][boardSize];
 
         int n = sc.nextInt();
