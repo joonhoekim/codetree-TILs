79737de7af15d2a1bf9d3c8424352e24dbecb944
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-29 21:41:42 +0900

    Title: 연속되는 수 4, Time: 167ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240229/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int prevNumber = 0;
+        int currentNumber = 0;
+        int count = 1;
+        int max = 1;
+        for(int i=0;i<n;i++) {
+            currentNumber=sc.nextInt();
+            if(i==0) {
+                prevNumber = currentNumber;
+                continue;
+            }
+
+            if(prevNumber<currentNumber) {
+                max = Math.max(++count,max);
+            } else {
+                count = 1;
+            }
+            prevNumber = currentNumber;
+        }
+        System.out.print(max);
+    }
+}
