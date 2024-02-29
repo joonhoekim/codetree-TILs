748f7fbc383febcb9ccb29ef82c7bb4bcac1a588
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-02-29 20:46:48 +0900

    Title: 연속되는 수 2, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240229/연속되는 수 2/continuous-number2.java
+++ 240229/연속되는 수 2/continuous-number2.java
@@ -14,11 +14,11 @@ public class Main {
         //counting
         int cnt=1;
         int max=1;
-        for(int i=0; i<n-1; i++) {
+        for(int i=0; i<n; i++) {
             // 1. i 와 i-1 비교
             // 2. i==0 인 경우? 무조건 길이 1 시작
             // 7 7 7 2
-            if(arr[i]==arr[i+1]) {
+            if(i==0 || arr[i]==arr[i-1]) {
                 ++cnt;
                 if(max<=cnt) max = cnt;
             } else {
