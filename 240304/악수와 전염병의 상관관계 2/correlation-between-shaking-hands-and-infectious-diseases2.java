43ff3125a72ebabd9d5866f0bffaa705bb5286a0
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-04 03:46:51 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 189ms, Memory: 12MB, Status: Passed - CodeTree

--- 240304/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 240304/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -22,7 +22,7 @@ public class Main {
     public static void main(String[] args) {
         int n = sc.nextInt(); //인원 수
         int k = sc.nextInt(); //최대 전파 회수
-        int p = sc.nextInt(); //최초 감염자 번호
+        int p = sc.nextInt() -1; //최초 감염자 번호 toIdx
         int t = sc.nextInt(); //전체 악수 회수
         
         //1.setting
@@ -30,12 +30,12 @@ public class Main {
         for(int i=0; i<n; i++) {
             devs[i] = new Developer();
         }
-        devs[p-1].isInfected = true;
+        devs[p].isInfected = true;
         Handshaking[] handshakings = new Handshaking[t];
         for(int i=0; i<t; i++) {
             int time = sc.nextInt();
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+            int x = sc.nextInt() -1 ; //to Idx
+            int y = sc.nextInt() -1 ;
             handshakings[i] = new Handshaking(time, x, y);
         }
         Comparator<Handshaking> compByTime = new Comparator<Handshaking>() {
@@ -49,25 +49,39 @@ public class Main {
         //2.simulation
         for(int i=0; i<t; i++) {
 
-            // 처음엔 아래 조건보다 단순하게 x, y 각각에 대해서 수행했는데
-            // 그렇게하니까 전파 횟수를 두번 증가시키는 로직 오류가 발생..
+            //만약 둘 다 비감염이면 아무것도 안해도 됨. continue;
+            //만약 둘 다 감염이면 
+            // - 둘 다 전파제한 이상인 경우 continue;
+            // - 둘 다 전파제한 미만인 경우 둘 다 늘려주고 continue;
+            // - 하나만 전파제한 남은 경우 굳이 비교하기보다 둘 다 ++하고 continue;
+            // ㄴ 생각해보니 그냥 다 ++ 해주면 되겠네
             
-            if( devs[handshakings[i].x-1].isInfected ) {
-                if( devs[handshakings[i].x-1].spreadCount<=k ) {
-                    if( devs[handshakings[i].y-1].isInfected ) {
-                        devs[handshakings[i].x-1].spreadCount++;
-                        devs[handshakings[i].y-1].spreadCount++;
-                    } else {
-                        devs[handshakings[i].y-1].isInfected = true;
-                        devs[handshakings[i].x-1].spreadCount++;
-                        
-                    }
-                } else {
-                    if( devs[handshakings[i].y-1].isInfected ) {
-                        devs[handshakings[i].y-1].spreadCount++;
-                    }
-                }
+            //이제 한 쪽만 감염인 상태가 남음
+            //감염된 쪽의 전파수 없으면 continue;
+            //감염된 쪽의 전파수 남았으면 반대감염 및 전파수 증가
+
+
+            //둘 다 비감염
+            if( !devs[handshakings[i].x].isInfected && !devs[handshakings[i].y].isInfected ) {
+                continue;
             }
+
+            //둘 다 감염
+            if( devs[handshakings[i].x].isInfected && devs[handshakings[i].y].isInfected ) {
+                devs[handshakings[i].x].spreadCount++;
+                devs[handshakings[i].y].spreadCount++;
+                continue;
+            }
+
+            //한쪽만 감염된 경우
+            if( devs[handshakings[i].x].isInfected && devs[handshakings[i].x].spreadCount<k ) {
+                devs[handshakings[i].y].isInfected = true;
+                devs[handshakings[i].x].spreadCount++;
+            } else if ( devs[handshakings[i].y].isInfected && devs[handshakings[i].y].spreadCount<k ) {
+                devs[handshakings[i].x].isInfected = true;
+                devs[handshakings[i].y].spreadCount++;
+            }
+           
         }
 
         //3.print
