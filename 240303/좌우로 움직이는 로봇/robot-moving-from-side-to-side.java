ac6c6247f6e847c73989edc8b5dbccc578120045
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-03 17:54:44 +0900

    Title: 좌우로 움직이는 로봇, Time: 152ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240303/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -0,0 +1,54 @@
+//풀이. 인덱스=시간, 값=위치로 시뮬레이션
+
+import java.util.*;
+
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int maxRange=1_000_000;
+        int[] robotA = new int[maxRange];
+        int[] robotB = new int[maxRange];
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        execCommands(robotA, n);
+        execCommands(robotB, m);
+        countEncounter(robotA, robotB, maxRange);      
+    }
+
+    static void execCommands(int[] timetable, int noc) { //number of commandlines
+        int time=0;
+        int v=1;
+        for(int commandIdx=0; commandIdx<noc; commandIdx++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0);
+            if(d=='R') {
+                while(t-->0) {
+                    timetable[time+1] = timetable[time] + v;
+                    time++;
+                }
+            } else if(d=='L') {
+                while(t-->0) {
+                    timetable[time+1] = timetable[time] - v;
+                    time++;
+                }
+            }
+        }
+    }
+
+    static void countEncounter(int[] a, int[] b, int searchRange) {
+        //encounter condition
+        //1. each position is not same.
+        int count=0;
+        for(int i=1; i<searchRange; i++) {
+            if(a[i-1] != b[i-1]) {
+                if(a[i] == b[i]) {
+                    count++;
+                }
+            }
+        }
+        
+        System.out.print(count);
+    }
+}
