c7bf7c2c38b98aaacb26127c176a30ccfef10274
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-05 19:45:38 +0900

    Title: 방향에 맞춰 이동, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240305/방향에 맞춰 이동/move-in-direction.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+    
+        int x=0;
+        int y=0;
+        int[] dx = new int[]{1,0,-1,0};
+        int[] dy = new int[]{0,-1,0,1};
+
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++) {
+            char dir = sc.next().charAt(0);
+            int dist = sc.nextInt();
+            int dirNum = -1;
+            if(dir=='E') {
+                dirNum = 0;
+            } else if (dir=='S') {
+                dirNum = 1;
+            } else if (dir=='W') {
+                dirNum = 2;
+            } else if (dir=='N') {
+                dirNum = 3;
+            }
+
+            while(dist-->0) {
+                x+=dx[dirNum];
+                y+=dy[dirNum];
+            }
+        }
+        System.out.print(x+" "+y);
+    }
+}
