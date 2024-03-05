3c7ced6d42d37599125689459e561f6a286573bc
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-06 01:34:46 +0900

    Title: 작은 구슬의 이동, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240306/작은 구슬의 이동/small-marble-movement.java
+++ 240306/작은 구슬의 이동/small-marble-movement.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
-        int[] dx = new int[]{-1,0,1,0};
-        int[] dy = new int[]{0,-1,0,1};
+        int[] drow = new int[]{-1,0,1,0};
+        int[] dcol = new int[]{0,-1,0,1};
 
         int n = sc.nextInt();
         int t = sc.nextInt();
@@ -12,17 +12,23 @@ public class Main {
         char dir = sc.next().charAt(0);
 
         int dirNum = -1;
+        //이 문제가 복잡한 이유는, 열은 좌-감소, 우-증가 그대로인데
+        //행만 내려오면 반대로 증가, 올라가면 반대로 감소이기 때문...
         switch(dir) {
+            //L: 열만 하나 감소
             case 'L': dirNum = 1; break;
-            case 'D': dirNum = 2; break;
+            //D: 행만 하나 감소
+            case 'D': dirNum = 0; break;
+            //R: 열만 하나 증가
             case 'R': dirNum = 3; break;
-            case 'U': dirNum = 0; break;
+            //U: 행만 하나 증가
+            case 'U': dirNum = 2; break;
         }
 
-        for(int i = 0; i<=t+1; i++) {
+        for(int second=0; second<=t+1; second++) {
             //이동 후 값이 내부 안에 들어가 있다면 이동한다.
-            int nrow = row + dx[dirNum];
-            int ncol = col + dy[dirNum];
+            int nrow = row + drow[dirNum];
+            int ncol = col + dcol[dirNum];
             if(isIn(n,nrow,ncol)) {
                 row = nrow;
                 col = ncol;
@@ -30,6 +36,7 @@ public class Main {
                 dirNum = (dirNum + 2) % 4;
             }
         }
+
         System.out.print(row+" "+col);
         
     }
