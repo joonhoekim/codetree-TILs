65b654505d24576e5b2d086612a5c440bacb39e0
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-06 01:43:22 +0900

    Title: 작은 구슬의 이동, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240306/작은 구슬의 이동/small-marble-movement.java
+++ 240306/작은 구슬의 이동/small-marble-movement.java
@@ -1,9 +1,10 @@
 import java.util.*;
 public class Main {
+    static Scanner sc =  new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
-        int[] drow = new int[]{-1,0,1,0};
-        int[] dcol = new int[]{0,-1,0,1};
+        
+        int[] dcol = new int[]{-1,0,1,0};
+        int[] drow = new int[]{0,-1,0,1};
 
         int n = sc.nextInt();
         int t = sc.nextInt();
@@ -12,17 +13,12 @@ public class Main {
         char dir = sc.next().charAt(0);
 
         int dirNum = -1;
-        //이 문제가 복잡한 이유는, 열은 좌-감소, 우-증가 그대로인데
-        //행만 내려오면 반대로 증가, 올라가면 반대로 감소이기 때문...
         switch(dir) {
             //L: 열만 하나 감소
-            case 'L': dirNum = 1; break;
-            //D: 행만 하나 감소
-            case 'D': dirNum = 0; break;
-            //R: 열만 하나 증가
-            case 'R': dirNum = 3; break;
-            //U: 행만 하나 증가
-            case 'U': dirNum = 2; break;
+            case 'L': dirNum = 0; break;
+            case 'D': dirNum = 3; break;
+            case 'R': dirNum = 2; break;
+            case 'U': dirNum = 1; break;
         }
 
         for(int second=0; second<=t+1; second++) {
@@ -33,7 +29,7 @@ public class Main {
                 row = nrow;
                 col = ncol;
             } else {
-                dirNum = (dirNum + 2) % 4;
+                dirNum = (dirNum+2)%4;
             }
         }
 
