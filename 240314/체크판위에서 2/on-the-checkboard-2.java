6b2056c8824564d5934c9a4ea87f125ad5f91105
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-14 00:25:26 +0900

    Title: 체크판위에서 2, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240314/체크판위에서 2/on-the-checkboard-2.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rowSize = sc.nextInt();
+        int colSize = sc.nextInt();
+        char[][] board = new char[rowSize][colSize];
+        for(int i=0; i<rowSize; i++) {
+            for(int j=0; j<colSize; j++) {
+                board[i][j] =sc.next().charAt(0);
+            }
+        }
+
+        char start = board[0][0];
+        char end = board[rowSize-1][colSize-1];
+
+        //시작, 끝이 같은 색이면 룰 따라 도착 불가함
+        if(start == end) {
+            System.out.print(0);
+            return;
+        }
+
+        int count = 0 ;
+        for(int i=1; i<rowSize-2; i++) {
+            for(int j=1; j<colSize-2; j++) {
+
+                for(int k=i+1; k<rowSize-1; k++) {
+                    for (int l=j+1; l<colSize-1; l++) {
+
+                        if(board[i][j] != start && board[k][j] != end) {
+                            count++;
+                        } 
+                    }
+                }
+                
+            }
+        }
+
+        System.out.print(count);
+    }
+}
