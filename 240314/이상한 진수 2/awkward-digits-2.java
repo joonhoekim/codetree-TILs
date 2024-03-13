e61a02bf16e0b11097a051628e1267aec88c8e02
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-14 00:59:57 +0900

    Title: 이상한 진수 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240314/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String binaryInput = sc.next(); 
+
+        //가장 첫번쨰 0을 찾고, 없으면 가장 맨 뒤 1을 0으로 바꾸는 전략이 안 통한다고요..?
+        //contains, replaceFirst 쓰면 가장 간단할 것 같은데... 이걸 기본 문법으로 구현하면
+        int idx = -1;
+        for(int i=binaryInput.length()-1; i>0; i--) {
+            if(binaryInput.charAt(i) == '0') {
+                idx = i;
+            }
+        }
+        String originBinary;
+        if(idx != -1) {
+            originBinary = binaryInput.substring(0,idx) + "1" + binaryInput.substring(idx+1,binaryInput.length());
+        } else {
+            originBinary = binaryInput.substring(0,binaryInput.length()) + "0";
+        }
+
+        int ans = Integer.parseInt(originBinary, 2);
+
+        System.out.print(ans);
+
+        
+    }
+}
