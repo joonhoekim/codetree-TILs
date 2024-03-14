6dfa9f21ccf8c2094332aa28855315def025d6d3
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-14 21:30:25 +0900

    Title: Carry 피하기 2, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240314/Carry 피하기 2/escaping-carry-2.java
+++ 240314/Carry 피하기 2/escaping-carry-2.java
@@ -14,7 +14,7 @@ public class Main {
         int max = -1;
         for(int i=0; i<n-2; i++) {
             for(int j=i+1; j<n-1; j++) {
-                for(int k=j+1; k<n-2; k++) {
+                for(int k=j+1; k<n; k++) {
                     boolean haveCarry = false;
 
                     for(int exponent=0; exponent<6; exponent++) {
