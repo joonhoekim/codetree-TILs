0308b36060520b1736562c8b5a04fde85f6fbcd8
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-16 20:07:03 +0900

    Title: 최고의 13위치 2, Time: 207ms, Memory: 10MB, Status: Passed - CodeTree

--- 240316/최고의 13위치 2/best-place-of-13-2.java
+++ 240316/최고의 13위치 2/best-place-of-13-2.java
@@ -16,38 +16,20 @@ public class Main {
     int maxSum=0;
     for(int i=0; i<n; i++) {
       for(int j=0; j<scanSize; j++) {
-        if(isOutOfBound(i,j+2,n,n)) {
-          continue;
-        }
-        int sum = board[i][j] + board[i][j+1] + board[i][j+2];
         for(int k=i; k<n; k++) {
           for(int l=j; l<scanSize; l++) {
             //겹치면 안됨
-            if(k==i) {
-              if(l==j || l==j+1 || l==j+2) {
-                continue;
-              }
-            }
-
-            if(isOutOfBound(k,l+2,n,n)) {
+            if(k==i && Math.abs(j-l)<=2) {
               continue;
             }
-            sum = sum + board[k][l] + board[k][l+1] + board[k][l+2];
-            maxSum = Math.max(maxSum, sum);
-            sum = board[i][j] + board[i][j+1] + board[i][j+2];
+            int sum1 = board[i][j] + board[i][j+1] + board[i][j+2];
+            int sum2 = board[k][l] + board[k][l+1] + board[k][l+2];
+            maxSum = Math.max(maxSum, sum1+sum2);
           }
         }
 
       }
     }
     System.out.print(maxSum);
-  }
-
-  static boolean isOutOfBound(int r, int c, int rCount, int cCount) {
-    if(0<=r && 0<=c && r<rCount && c<cCount) {
-      return false;
-    }
-
-    return true;
   }
 }
