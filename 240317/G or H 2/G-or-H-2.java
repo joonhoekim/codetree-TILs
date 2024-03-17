545dc0af5bbca8a6d8b06f054f831f4c61d13c06
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-17 21:17:54 +0900

    Title: G or H 2, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240317/G or H 2/G-or-H-2.java
+++ 240317/G or H 2/G-or-H-2.java
@@ -1,5 +1,10 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 class Person {
     int position;
@@ -12,17 +17,19 @@ class Person {
 }
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 사람 수
-        sc.nextLine(); // 개행 문자 처리
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine()); // 사람 수
 
         ArrayList<Person> people = new ArrayList<>(); // 사람들을 저장하는 리스트
 
         // 각 사람의 위치와 팻말 정보를 입력받아 Person 객체를 생성하여 리스트에 저장합니다.
         for (int i = 0; i < n; i++) {
-            int position = sc.nextInt(); // 위치
-            char character = sc.next().charAt(0); // 팻말
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int position = Integer.parseInt(st.nextToken()); // 위치
+            char character = st.nextToken().charAt(0); // 팻말
             people.add(new Person(position, character));
         }
 
@@ -60,7 +67,11 @@ public class Main {
             }
         }
 
-        System.out.println(maxSize); // 최대 사진 크기 출력
+        bw.write(maxSize + "\n"); // 최대 사진 크기 출력
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
     // 리스트에 주어진 위치에 사람이 있는지 확인하는 메서드
