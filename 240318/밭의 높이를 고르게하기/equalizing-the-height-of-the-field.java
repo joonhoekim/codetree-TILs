b73c20fdbccc090c6af0213c63791889ca2fbe3a
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-18 10:35:51 +0900

    Title: 밭의 높이를 고르게하기, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240318/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //length of area
+        int t = sc.nextInt(); //target length
+        int h = sc.nextInt(); //target height
+        
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        int minCost = 99999;
+        for(int start = 0; start <= n-t; start++ ) {
+            
+            int cost = 0;
+            for(int cursor = start; cursor < start+t; cursor++) {
+                cost += Math.abs(arr[cursor] - h);
+            }
+            minCost = Math.min(minCost, cost);
+        }
+
+        System.out.print(minCost);
+    }
+}
