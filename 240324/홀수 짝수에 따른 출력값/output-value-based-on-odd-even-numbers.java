ff353479de239380f456089dddab30204f7c55ca
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-03-24 15:08:41 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240324/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(getSumOfEvenOrOdd(n));
+    }
+
+    static int getSumOfEvenOrOdd(int n) {
+        if(n<=0) {
+            return 0;
+        }
+        return n+getSumOfEvenOrOdd(n-2);
+    }
+}
