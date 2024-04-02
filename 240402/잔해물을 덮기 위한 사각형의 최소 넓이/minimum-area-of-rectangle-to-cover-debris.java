a6b49dd2909a0daac21544a96ec470e4f3e076e0
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-04-02 23:13:38 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 236ms, Memory: 27MB, Status: Passed - CodeTree

--- 240402/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 240402/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -1,64 +1,86 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 class Rect {
-    int x1;
-    int y1;
-    int x2;
-    int y2;
+
+  int x1;
+  int x2;
+  int y1;
+  int y2;
 }
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int offset = 1000;
-        int boardSize = 1000+offset+1;
-        int[][] board = new int[boardSize][boardSize];
-
-        Rect[] rects = new Rect[2];
-
-
-        for(int i=0; i<2; i++) {
-            rects[i] = new Rect();
-            rects[i].x1 = sc.nextInt() + offset;
-            rects[i].y1 = sc.nextInt() + offset;
-            rects[i].x2 = sc.nextInt() + offset;
-            rects[i].y2 = sc.nextInt() + offset;
-        }
-
-        
-        for(int x=rects[0].x1; x<=rects[0].x2; x++) 
-            for(int y=rects[0].y1;y<=rects[0].y2; y++)
-                board[x][y] = 1;
-        
-        for(int x=rects[1].x1; x<=rects[1].x2; x++) 
-            for(int y=rects[1].y1;y<=rects[1].y2; y++)
-                board[x][y] = -1;
-
-        int minX = 2002;
-        int minY = 2002;
-        int maxX = 0;
-        int maxY = 0;
-        int count = 0;
-        for(int x=0; x<boardSize; x++) 
-            for(int y=0; y<boardSize; y++)
-                if(board[x][y]==1) {
-                    minX = Math.min(minX,x);
-                    minY = Math.min(minY,y);
-                    maxX = Math.max(maxX,x);
-                    maxY = Math.max(maxY,y);
-                    count ++;
-                }
-
-        // System.out.printf("%d %d %d %d\n",minX, minY, maxX, maxY);
-
-        if(count ==0 ) {
-            System.out.println(0);
-            return;
-        }
-        System.out.println(
-            (maxX - minX ) * (maxY - minY )
-        );
-        
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Rect[] rects = new Rect[2];
+    for (int i = 0; i < 2; i++) {
+      rects[i] = new Rect();
     }
+
+    int offset = 1000;
+    int boardSize = offset + 1000 + 1;
+    int[][] board = new int[boardSize][boardSize];
+
+    for (int i = 0; i < 2; i++) {
+      rects[i].x1 = sc.nextInt() + offset;
+      rects[i].y1 = sc.nextInt() + offset;
+      rects[i].x2 = sc.nextInt() + offset;
+      rects[i].y2 = sc.nextInt() + offset;
+    }
+
+    //첫번쨰 사각형을 1로 표현하기
+    for (int x = rects[0].x1; x < rects[0].x2; x++) {
+      for (int y = rects[0].y1; y < rects[0].y2; y++) {
+        //칠해진 보드 좌표는 보드 격자의 왼쪽 아래 좌표를 의미한다.
+        board[x][y] = 1;
+      }
+    }
+
+    //두번째 사각형을 -1로 표현하기
+    for (int x = rects[1].x1; x < rects[1].x2; x++) {
+      for (int y = rects[1].y1; y < rects[1].y2; y++) {
+        //칠해진 보드 좌표는 보드 격자의 왼쪽 아래 좌표를 의미한다.
+        board[x][y] = -1;
+      }
+    }
+
+    //최대 좌표와 최소 좌표 구하기
+    int count = 0;
+    int minX = 2000;
+    int maxX = 0;
+    int minY = 2000;
+    int maxY = 0;
+    for (int x = 0; x < boardSize; x++) {
+      for (int y = 0; y < boardSize; y++) {
+        if (board[x][y] == 1) {
+          count++;
+          minX = Math.min(minX, x);
+          maxX = Math.max(maxX, x);
+          minY = Math.min(minY, y);
+          maxY = Math.max(maxY, y);
+        }
+      }
+
+    }
+
+    int ans = 0;
+    //남은 잔해가 없다면 다 덮인 것이므로 0이다.
+    if (count == 0) {
+      ans = 0;
+    }
+    //만약 1개만 나왔다면 넓이가 1이다.
+    else if (count == 1) {
+      ans = 1;
+    }
+    //여러개 나왔다면, 최소/최대값 이용해서 답을 구한다.
+    else {
+      ans = (maxX - minX+1) * (maxY - minY+1);
+    }
+
+    System.out.println(ans);
+
+
+  }
+
 }
