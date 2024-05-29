02b8120500592a0733d41258cbac2798bcf1c613
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-05-29 15:06:53 +0900

    Title: L, R 그리고 B, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240529/L, R 그리고 B/l-r-and-b.java
@@ -0,0 +1,77 @@
+// 최단 경로 구하기
+
+// 직선으로 이어지는 경우를 제외하면, 최단경로 길이에 R이 영햐을 미치지 않는다.
+// 이 점을 활용하면 실제로 도달하는 것을 시뮬레이션하지 않아도 될 것이다.
+
+import java.util.Scanner;
+
+class RC {
+    int row;
+    int col;
+
+    RC(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int boardWidth = 10;
+        int boardHeight = 10;
+        char[][] board = new char[boardHeight][boardWidth];
+
+        RC l = null;
+        RC r = null;
+        RC b = null;
+
+
+        for (int row = 0; row < boardHeight; row++) {
+            board[row] = sc.nextLine().toCharArray();
+
+            for (int col = 0; col < boardWidth; col++) {
+                if (board[row][col] == 'L') {
+                    l = new RC(row, col);
+                } else if (board[row][col] == 'R') {
+                    r = new RC(row, col);
+                } else if (board[row][col] == 'B') {
+                    b = new RC(row, col);
+                }
+            }
+        }
+
+        if (isStraight(l, b) && isBlocked(l, r, b)) { // && 문법에 따라 isStraight를 먼저 검사한다!
+            System.out.println(getMinDiagonalDist(l, b) + 2);
+        } else {
+            System.out.println(getMinDiagonalDist(l, b));
+        }
+
+
+    }
+
+    private static boolean isBlocked(RC l, RC r, RC b) {
+        if (l.row == b.row && b.row == r.col) {
+          return true;
+        }
+
+        else if (l.col == b.col && b.col == r.col) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+
+    private static int getMinDiagonalDist(RC l, RC b) {
+        int distHeight = Math.abs(l.row - b.row);
+        int distWidth = Math.abs(l.col - b.col);
+        return distHeight + distWidth - 1;
+    }
+
+
+    private static boolean isStraight(RC l, RC b) {
+        return l.row == b.row || l.col == b.col;
+    }
+}
