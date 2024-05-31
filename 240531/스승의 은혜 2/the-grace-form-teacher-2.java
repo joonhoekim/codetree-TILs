fe35a4268d14cfee9adbab79cb4bc6f089d14176
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-05-31 17:42:44 +0900

    Title: 스승의 은혜 2, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240531/스승의 은혜 2/the-grace-form-teacher-2.java
+++ 240531/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -14,26 +14,31 @@ public class Main {
         }
 
         // i번째 물건에 쿠폰을 적용해보고 몇개 구매할 수 있는지 확인
-        // O(n^2) 일 것이다.
         int globalMaxBuyIdx = 0;
         for (int discounted = 0; discounted < n; discounted++) {
-            for (int buyedIdx = 0; buyedIdx < n; buyedIdx++) {
-                int localBudget = budget;
+            int localBudget = budget;
 
+            for (int buyedIdx = 0; buyedIdx < n; buyedIdx++) {
                 if(buyedIdx==discounted) {
-                    budget -= prices[buyedIdx]/2;
+                    localBudget -= prices[buyedIdx]/2;
                 } else {
-                    budget -= prices[buyedIdx];
+                    localBudget -= prices[buyedIdx];
                 }
 
                 //예산 소진
-                if(budget<0) {
+                if(localBudget<0) {
+                    globalMaxBuyIdx = Math.max(globalMaxBuyIdx, buyedIdx-1);
+                    break;
+                }
+
+                //만약 전부 구매했다면..
+                if(buyedIdx == n-1) {
                     globalMaxBuyIdx = Math.max(globalMaxBuyIdx, buyedIdx);
+                    break;
                 }
             }
         }
 
-        //Idx 에서 +1 안해줘도 된다. buyed 는 못산 것이니까..
-        System.out.println(globalMaxBuyIdx);
+        System.out.println(globalMaxBuyIdx+1);
     }
 }
