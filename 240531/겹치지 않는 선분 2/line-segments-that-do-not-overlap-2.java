6107db03b2f60c40965b342c1c3c24bd2ad4adf2
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-05-31 17:17:42 +0900

    Title: 겹치지 않는 선분 2, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240531/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
+++ 240531/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 class Line {
     int startX;
+    //    int startY = 0;
     int endX;
-    boolean overwraped = false;
+    //    int endY = 1;
+    boolean overwrap = false;
 
     Line(int startX, int endX) {
         this.startX = startX;
@@ -24,23 +26,23 @@ public class Main {
         }
 
         for (int i = 0; i < n; i++) {
-            //겹침 상태에 있다면 이 선분을 포함하는 다른 선분이 있으니 볼 필요가 없다.
-            if (lines[i].overwraped) continue;
             for (int j = 0; j < n; j++) {
                 //자기 자신은 검사대상이 아니다.
                 if (i == j) continue;
-                if (lines[j].overwraped) continue;
+                //겹침 상태에 있다면 이 선분을 포함하는 다른 선분이 있으니 볼 필요가 없다.
+                //단 겹침 상태에 있는 선분[i]도 다른 추가 선분[j]들과 겹칠 수 있으므로 검사는 해야 한다.
+                if (lines[j].overwrap) continue;
 
                 if (isOverwrapping(lines[i], lines[j])) {
-                    lines[i].overwraped = true;
-                    lines[j].overwraped = true;
+                    lines[i].overwrap = true;
+                    lines[j].overwrap = true;
                 }
             }
         }
 
         int count = 0;
         for (int i = 0; i < n; i++) {
-            if(!lines[i].overwraped) {
+            if (!lines[i].overwrap) {
                 count++;
             }
         }
@@ -50,6 +52,14 @@ public class Main {
     }
 
     private static boolean isOverwrapping(Line wrapper, Line wrapped) {
-        return wrapper.startX <= wrapped.startX && wrapped.endX <= wrapper.endX;
+        //조건상 입력에서 시작,끝점이 겹쳐지지 않는다고 하니 고려하지 않음
+        if (wrapped.startX < wrapper.startX) {
+            if (wrapped.endX >= wrapper.endX) return true;
+        } else {
+            if (wrapped.endX <= wrapper.endX) return true;
+        }
+
+
+        return false;
     }
 }
