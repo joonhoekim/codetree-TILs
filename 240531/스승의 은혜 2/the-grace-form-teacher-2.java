bf8da56a2c0642dfac27b1b7cda15d5d66e49bf2
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-05-31 17:50:51 +0900

    Title: 스승의 은혜 2, Time: 152ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240531/스승의 은혜 2/the-grace-form-teacher-2.java
+++ 240531/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 //최대 개수를 확보하기 위한 탐색문제
 //1회의 반값쿠폰이 있다. 주어진 예산에서 최대한 구매횟수를 늘리려면?
+//선물을 학생들 순서를 고려해서 구매할 필요가 없다!
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -9,17 +11,19 @@ public class Main {
         int n = sc.nextInt();
         int budget = sc.nextInt();
         int[] prices = new int[n];
+        //학생 순서를 고려할 필요가 없으므로, 저렴한 것부터 사주자.
+        Arrays.sort(prices);
         for (int i = 0; i < n; i++) {
             prices[i] = sc.nextInt();
         }
 
         // i번째 물건에 쿠폰을 적용해보고 몇개 구매할 수 있는지 확인
         int globalMaxBuyIdx = 0;
-        for (int discounted = 0; discounted < n; discounted++) {
+        for (int discountIdx = 0; discountIdx < n; discountIdx++) {
             int localBudget = budget;
 
             for (int buyedIdx = 0; buyedIdx < n; buyedIdx++) {
-                if(buyedIdx==discounted) {
+                if(buyedIdx==discountIdx) {
                     localBudget -= prices[buyedIdx]/2;
                 } else {
                     localBudget -= prices[buyedIdx];
