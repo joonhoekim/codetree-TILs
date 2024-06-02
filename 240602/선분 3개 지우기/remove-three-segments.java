1ee2614c43cbeb2a4d26bac32bbfbc5aad57b1ef
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-06-02 17:07:16 +0900

    Title: 선분 3개 지우기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240602/선분 3개 지우기/remove-three-segments.java
+++ 240602/선분 3개 지우기/remove-three-segments.java
@@ -27,10 +27,6 @@ public class Main {
 
   public static void main(String[] args) {
     int lineCount = sc.nextInt();
-    if(lineCount == 4) {
-      System.out.println(4);
-      return;
-    }
     Line[] lines = new Line[lineCount];
     for (int i = 0; i < lineCount; i++) {
       lines[i] = new Line(sc.nextInt(), sc.nextInt());
@@ -42,10 +38,12 @@ public class Main {
     for (int i = 0; i < lines.length - 2; i++) {
       for (int j = i + 1; j < lines.length - 1; j++) {
         for (int k = j + 1; k < lines.length; k++) {
-          int[] excluded = new int[]{i, j, k};
-          if (checkUnoverwrappedWithExclusion(lines, excluded)) {
+          //그 3가지 제거한 경우 중복이 없는지 확인하자.
+          int[] excludedIdx = new int[]{i, j, k};
+          if (checkUnoverwrappedWithExclusion(lines, excludedIdx)) {
             answer++;
           }
+
         }
       }
     }
@@ -53,22 +51,27 @@ public class Main {
     System.out.println(answer);
   }
 
-  private static boolean checkUnoverwrappedWithExclusion(Line[] lines, int[] excluded) {
+  private static boolean checkUnoverwrappedWithExclusion(Line[] lines, int[] excludedIdx) {
 
     //선분 둘을 짝지어 Overwrap 여부를 확인한다.
     for (int line1Idx = 0; line1Idx < lines.length; line1Idx++) {
-      loop: for (int line2Idx = 0; line2Idx < lines.length; line2Idx++) {
+      loop:
+      for (int line2Idx = 0; line2Idx < lines.length; line2Idx++) {
+        if(line1Idx==line2Idx) continue;
+
         //excluded lineIdx은 제외한다.
-        for (int i = 0; i < excluded.length; i++) {
-          if (line1Idx == excluded[i] || line2Idx == excluded[i]) {
-            break loop;
+        for (int i = 0; i < excludedIdx.length; i++) {
+          if (line1Idx == excludedIdx[i] || line2Idx == excludedIdx[i]) {
+            continue loop;
           }
         }
 
         boolean seperated =
             (lines[line1Idx].endX < lines[line2Idx].startX) || (lines[line1Idx].startX > lines[line2Idx].endX);
 
-        if(!seperated) return false;
+        if (!seperated) {
+          return false;
+        }
 
 
       }
