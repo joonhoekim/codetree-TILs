1a768ee8c2c62b03d5ad25469927e0a920b82fc8
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-06-05 17:03:39 +0900

    Title: 스승의 은혜 3, Time: 708ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240605/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -0,0 +1,78 @@
+//이전에 풀었던 문제와 동일한데, 배송비 조건이 추가되었음
+//경계: 하나도 못 살때, 제한된 수를 살 수 있을 때, 다 살수 있을 때
+//clone()은 얕은 복사, 별도 구현 해줘야 한다.
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class Cost implements Cloneable{
+    int price;
+    int shippingCost;
+
+    Cost(int price, int shippingCost) {
+        this.price = price;
+        this.shippingCost = shippingCost;
+    }
+
+    @Override
+    public Object clone() {
+        Object obj = null;
+        try {
+            obj = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return obj;
+    }
+    
+}
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int studentCount = sc.nextInt();
+        int budget = sc.nextInt();
+        Cost[] costs = new Cost[studentCount];
+
+        for (int i = 0; i < studentCount; i++) {
+            int price = sc.nextInt();
+            int shippingCost = sc.nextInt();
+            costs[i] = new Cost(price, shippingCost);
+        }
+
+        Comparator<Cost> totalPriceAsc = new Comparator<Cost>() {
+            @Override
+            public int compare(Cost o1, Cost o2) {
+                return (o1.price + o1.shippingCost) - (o2.price + o2.shippingCost);
+            }
+        };
+
+
+        int maxBuyCount = 0;
+        for (int i = 0; i < costs.length; i++) {
+//            System.out.printf("---할인idx: %d, 할인전 가격: %d ---\n",i,costs[i].price);
+
+            Cost[] costCloned = costs.clone();
+            costCloned[i].price = costCloned[i].price / 2;
+            Arrays.sort(costCloned, totalPriceAsc);
+            int totalPrice = 0;
+            int boughtCount = 0;
+            for (int j = 0; j < costCloned.length; j++) {
+                int cost = costCloned[j].price + costCloned[j].shippingCost;
+                totalPrice += cost;
+
+                if (totalPrice > budget) {
+                    break;
+                }
+                boughtCount++;
+            }
+            maxBuyCount = Math.max(maxBuyCount, boughtCount);
+        }
+
+        System.out.println(maxBuyCount);
+
+    }
+}
