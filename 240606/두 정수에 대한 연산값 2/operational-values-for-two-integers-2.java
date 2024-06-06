ef6bd1bced9a6a8dd272df90b92c27b358f011ad
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-06-06 20:15:15 +0900

    Title: 두 정수에 대한 연산값 2, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240606/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class IntWrapper {
+  int value;
+
+  public IntWrapper(int value) {
+    this.value = value;
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    IntWrapper a = new IntWrapper(sc.nextInt());
+    IntWrapper b = new IntWrapper(sc.nextInt());
+
+    calcValue(a, b);
+
+    System.out.println(a.value + " " + b.value);
+    sc.close();
+  }
+
+  public static void calcValue(IntWrapper a, IntWrapper b) {
+    if (a.value > b.value) {
+      a.value *= 2;
+      b.value += 10;
+    } else {
+      b.value *= 2;
+      a.value += 10;
+    }
+  }
+}
