28bbac95578d9a0be10005b8de8368eece72ffef
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-06-08 11:53:59 +0900

    Title: 등차수열, Time: 136ms, Memory: 9MB, Status: Passed - CodeTree

--- 240608/등차수열/arithmetic-sequence.java
+++ 240608/등차수열/arithmetic-sequence.java
@@ -1,22 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int n = Integer.parseInt(br.readLine());
     int[] arr = new int[n];
     int[] arrCloned = new int[n];
+    StringTokenizer st = new StringTokenizer(br.readLine());
     for(int i=0; i<n; i++) {
-      arr[i] = sc.nextInt();
+      arr[i] = Integer.parseInt(st.nextToken());
       arrCloned[i] = arr[i];
     }
-    
+
     Arrays.sort(arrCloned);
     int min = arrCloned[0];
     int max = arrCloned[n-1];
-    
+
     int globalSatisfiedCount = 0;
     for(int k=min+1; k<max; k++) {
       int localSatisfiedCount = 0;
@@ -31,6 +38,9 @@ public class Main {
         globalSatisfiedCount = localSatisfiedCount;
       }
     }
-    System.out.println(globalSatisfiedCount);
+    bw.write(String.valueOf(globalSatisfiedCount));
+    bw.flush();
+    bw.close();
+    br.close();
   }
 }
