e0f068bd4997d96514954271f31c29e045621154
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-06-11 20:35:27 +0900

    Title: 선택 정렬 구현, Time: 211ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240611/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    selectionSort(arr);
+
+    for (int i = 0; i < n; i++) {
+      System.out.print(arr[i] + " ");
+    }
+  }
+
+  static void selectionSort(int[] arr) {
+    for(int i = 0; i < arr.length; i++) {
+      int minIdx = i;
+      int minVal = arr[i];
+      for(int j = i; j < arr.length; j++) {
+        if(arr[minIdx] > arr[j]) {
+          minIdx = j;
+          minVal = arr[j];
+        }
+      }
+      int temp = arr[i];
+      arr[i] = arr[minIdx];
+      arr[minIdx] = temp;
+    }
+  }
+
+}
