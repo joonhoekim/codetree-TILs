85f31e96f22007dcb2fe989178e6bff0c785bd7f
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-06-13 18:54:56 +0900

    Title: 오름 내림차순 정렬, Time: 332ms, Memory: 16MB, Status: Passed - CodeTree

--- 240613/오름 내림차순 정렬/inc-dec-sorting.java
+++ 240613/오름 내림차순 정렬/inc-dec-sorting.java
@@ -10,25 +10,36 @@ public class Main {
     int n = sc.nextInt();
     int[] arr = new int[n];
 
-
     for (int i = 0; i < n; i++) {
       arr[i] = sc.nextInt();
     }
 
-    // 메서드 체인+람다 함수가 적용됨, Integer[]로 변환
-    Integer[] descArr = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+    asc(arr);
+    desc(arr);
 
+    sc.close();
+  }
+
+  public static void asc(int[] arr) {
+    // 오름차순 정렬
     Arrays.sort(arr);
-    Arrays.sort(descArr, Collections.reverseOrder());
 
     for (int value : arr) {
       System.out.printf("%d ", value);
     }
     System.out.println();
-    for (int i = 0; i < n; i++) {
+  }
+
+  public static void desc(int[] arr) {
+    // 메서드 체인+람다 함수가 적용됨, Integer[]로 변환
+    Integer[] descArr = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+
+    // 내림차순 정렬
+    Arrays.sort(descArr, Collections.reverseOrder());
+
+    for (int i = 0; i < arr.length; i++) {
       System.out.printf("%d ", descArr[i]);
     }
-
-    sc.close();
+    System.out.println();
   }
 }
