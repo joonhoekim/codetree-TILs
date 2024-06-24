7194a44cb3d1e257f97858aa3d1499a259ffcaaa
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-06-24 13:46:48 +0900

    Title: 사는 지역, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240624/사는 지역/where-live.java
+++ 240624/사는 지역/where-live.java
@@ -24,7 +24,13 @@ public class Main {
       people[i] = getPerson(sc);
     }
 
-    int idx = getLatest(people);
+    int idx = 0;
+
+    for (int i = 0; i < people.length; i++) {
+      if (people[idx].name.compareTo(people[i].name) < 0) {
+        idx = i;
+      }
+    }
 
     System.out.println("name " + people[idx].name);
     System.out.println("addr " + people[idx].address);
@@ -33,16 +39,5 @@ public class Main {
 
   public static Person getPerson(Scanner sc) {
     return new Person(sc.next(), sc.next(), sc.next());
-  }
-
-  public static int getLatest(Person[] people) {
-    int latestIndex = 0;
-
-    for (int i = 0; i < people.length-1; i++) {
-      if (people[i].name.compareTo(people[i+1].name) > 0) {
-        latestIndex = i;
-      } 
-    }
-    return latestIndex;
   }
 }
