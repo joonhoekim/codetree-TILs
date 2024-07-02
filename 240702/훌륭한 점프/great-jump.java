97eada009c35b5a61ec04437b3c4849ff8e9e047
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-07-02 23:51:22 +0900

    Title: 훌륭한 점프, Time: 155ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240702/훌륭한 점프/great-jump.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Main {
+
+    static final int MAX_VAL = 100;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //stone bridge, number of stones.
+        int k = sc.nextInt(); //max jump length.
+        
+        int[] arr = new int[n];
+        int[] arrCopied = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int outerMin = Integer.MAX_VALUE;
+        for(int innerMax=MAX_VAL; innerMax >= Math.max(arr[0], arr[n-1]); innerMax--) {
+            if(isPossible(innerMax, arr))
+                outerMin = Math.min(outerMin, innerMax);
+        }
+
+        System.out.println(outerMin);
+    }
+
+    static boolean isPossible(int innerMax, int[] arr) {
+        int[] availableIndexes = new int[arr.length];
+        int count = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] <= innerMax) {
+                availableIndexes[count++] = i;
+            }
+        }
+
+        for(int i=1; i<count; i++) {
+            int dist = availableIndexes[i] - availableIndexes[i-1];
+            if(dist>2) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
