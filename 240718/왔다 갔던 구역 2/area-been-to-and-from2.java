ac64038e7b0397217ebad86f5d8665aefcf89123
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-07-18 16:41:09 +0900

    Title: 왔다 갔던 구역 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240718/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240718/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -5,26 +5,28 @@ public class Main {
     Scanner sc = new Scanner(System.in);
 
     int n = sc.nextInt();
-    int[] area = new int[2000];
-    int curPoint = 1000;
+    int boardSize = 100*10*2+2;
+    int[] area = new int[boardSize];
+    int curPoint = boardSize/2;
+
     for (int i = 0; i < n; i++) {
       int x = sc.nextInt();
       String way = sc.next();
 
 
       if (way.equals("R")) {
-        for (int j = curPoint; j <= curPoint + x; j++) {
+        for (int j = curPoint; j < curPoint+x; j++) {
           area[j]++;
         }
         curPoint += x;
       } else if (way.equals("L")) {
-        for (int j = curPoint; j >= curPoint-x; j--) {
+        for (int j = curPoint; j > curPoint-x; j--) {
           area[j]++;
         }
         curPoint -= x;
       }
     }
-    int visitedArea = 0;
+    int visitedArea = 1;
 
     for (int i = 0; i < area.length; i++) {
       if (area[i] > 1 && area[i+1] > 1) {
