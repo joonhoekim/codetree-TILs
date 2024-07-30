412f7deef6a3efc1de4f086d0ee859de3c03c74f
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-07-30 13:26:14 +0900

    Title: 구간 잘 나누기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240730/구간 잘 나누기/divide-sections-well.java
+++ 240730/구간 잘 나누기/divide-sections-well.java
@@ -1,61 +1,49 @@
 import java.util.Scanner;
 
 public class Main {
+    static int n, m;
+    static int[] arr;
 
-  static int n, m;
-  static int[] arr;
-  static int minMaxSum = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    n = sc.nextInt();
-    m = sc.nextInt();
+        arr = new int[n];
+        int left = 0, right = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            right += arr[i];
+            left = Math.max(left, arr[i]);
+        }
 
-    arr = new int[n];
-    for (int i = 0; i < n; i++) {
-      arr[i] = sc.nextInt();
+        System.out.println(binarySearch(left, right));
     }
 
-    boolean[] dividers = new boolean[n - 1];
-    generateCombinations(dividers, 0, m - 1);
-
-    System.out.println(minMaxSum);
-  }
-
-  static void generateCombinations(boolean[] dividers, int start, int remainingDividers) {
-    if (remainingDividers == 0) {
-      int maxSum = calculateMaxSum(dividers);
-      minMaxSum = Math.min(minMaxSum, maxSum);
-      return;
+    static int binarySearch(int left, int right) {
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (canDivide(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 
-    for (int i = start; i < dividers.length - remainingDividers + 1; i++) {
-      dividers[i] = true;
-      generateCombinations(dividers, i + 1, remainingDividers - 1);
-      dividers[i] = false;
+    static boolean canDivide(int maxSum) {
+        int count = 1;
+        int currentSum = 0;
+        for (int num : arr) {
+            if (currentSum + num > maxSum) {
+                count++;
+                currentSum = num;
+                if (count > m) return false;
+            } else {
+                currentSum += num;
+            }
+        }
+        return true;
     }
-  }
-
-  static int calculateMaxSum(boolean[] dividers) {
-    int maxSum = 0;
-    int currentSum = 0;
-    int groupCount = 0;
-
-    for (int i = 0; i < n; i++) {
-      currentSum += arr[i];
-
-      if (i == n - 1 || dividers[i]) {
-        maxSum = Math.max(maxSum, currentSum);
-        currentSum = 0;
-        groupCount++;
-      }
-    }
-
-    // m개의 그룹으로 정확히 나누어지지 않으면 무효한 경우
-    if (groupCount != m) {
-      return Integer.MAX_VALUE;
-    }
-
-    return maxSum;
-  }
 }
