bf77153d2dc4216cc7b031e8b62b97d879cc151c
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-07-30 15:32:13 +0900

    Title: 구간 잘 나누기, Time: 167ms, Memory: 10MB, Status: Passed - CodeTree

--- 240730/구간 잘 나누기/divide-sections-well.java
+++ 240730/구간 잘 나누기/divide-sections-well.java
@@ -1,49 +1,68 @@
 import java.util.Scanner;
 
 public class Main {
-    static int n, m;
-    static int[] arr;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
+  //수열 원소의 최댓값은 10000
+  private static final int MAX_ANSWER = 10000;
 
-        arr = new int[n];
-        int left = 0, right = 0;
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-            right += arr[i];
-            left = Math.max(left, arr[i]);
-        }
+  //수열 최대 길이는 100
+  private static final int MAX_N = 100;
 
-        System.out.println(binarySearch(left, right));
+  //수열 길이n, 구간 개수 m
+  private static int n, m;
+
+  //수열 저장할 배열
+  private static int[] arr;
+
+  public static void main(String[] args) {
+    //값을 입력받는다.
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    m = sc.nextInt();
+
+    arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
     }
 
-    static int binarySearch(int left, int right) {
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (canDivide(mid)) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+    //구간의 최댓값을 결정한 다음 탐색한다!
+    //구간의 최댓값을 1부터 MAX_ANSWER까지 가정해서,
+    //그것이 가능한지를 확인해보고, 가능한 경우 그것이 최댓값의 최소가 됨을 이용한다.
+    int ans = Integer.MAX_VALUE;
+    for (int assumedAnswer = 1; assumedAnswer < MAX_ANSWER; assumedAnswer++) {
+      boolean isSatisfied = true; // 1개의 원소가 assumedAnswer를 넘는 경우, m개를 넘는 세그먼트가 요구되는 경우에 쓰는 플래그다.
+      int countSegments = 1; // 나누지 않아도, 세그먼트는 1개부터다.
+      int localSum = 0;
+
+      //각 자리수에 대해서 검사를 실시한다.
+      for (int checkedIdx = 0; checkedIdx < n; checkedIdx++) {
+        //원소 1개인 구간조차 예상최댓값을 넘어가는 경우는 fail이다.
+        if (arr[checkedIdx] > assumedAnswer) {
+          isSatisfied = false;
+          break;
         }
-        return left;
+
+        if (localSum + arr[checkedIdx] > assumedAnswer) {
+          localSum = 0;
+          countSegments++;
+        }
+
+        if (countSegments > m) {
+          isSatisfied = false;
+          break;
+        }
+
+        localSum += arr[checkedIdx];
+
+      }
+
+      //조건 플래그 검사한다.
+      if (isSatisfied) {
+        System.out.println(assumedAnswer);
+        return;
+      }
+
     }
 
-    static boolean canDivide(int maxSum) {
-        int count = 1;
-        int currentSum = 0;
-        for (int num : arr) {
-            if (currentSum + num > maxSum) {
-                count++;
-                currentSum = num;
-                if (count > m) return false;
-            } else {
-                currentSum += num;
-            }
-        }
-        return true;
-    }
+  }
 }
