7e773dee144cc3fe894395948d24d8d77afbadb4
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-07-30 17:45:07 +0900

    Title: 최대 최소간의 차, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240730/최대 최소간의 차/difference-between-maximum-and-minimum.java
+++ 240730/최대 최소간의 차/difference-between-maximum-and-minimum.java
@@ -1,59 +1,43 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken());
-    int k = Integer.parseInt(st.nextToken());
-
-    int[] arr = new int[n];
-    st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < n; i++) {
-      arr[i] = Integer.parseInt(st.nextToken());
-    }
-
-    Arrays.sort(arr);
-
-    int left = 0, right = 0;
-    int maxLength = 0;
-    int bestLeft = 0, bestRight = 0;
-
-    while (right < n) {
-      if (arr[right] - arr[left] <= k) {
-        if (right - left + 1 > maxLength) {
-          maxLength = right - left + 1;
-          bestLeft = left;
-          bestRight = right;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        right++;
-      } else {
-        left++;
-      }
+        
+        Arrays.sort(arr);
+        
+        int left = 0, right = 0;
+        int maxLength = 0;
+        int bestLeft = 0, bestRight = 0;
+        
+        while (right < n) {
+            if (arr[right] - arr[left] <= k) {
+                if (right - left + 1 > maxLength) {
+                    maxLength = right - left + 1;
+                    bestLeft = left;
+                    bestRight = right;
+                }
+                right++;
+            } else {
+                left++;
+            }
+        }
+        
+        long cost = 0;
+        for (int i = 0; i < bestLeft; i++) {
+            cost += arr[bestLeft] - arr[i];
+        }
+        for (int i = bestRight + 1; i < n; i++) {
+            cost += arr[i] - arr[bestRight];
+        }
+        
+        System.out.println(cost);
     }
-
-    long cost = 0;
-    for (int i = 0; i < bestLeft; i++) {
-      cost += arr[bestLeft] - arr[i];
-    }
-    for (int i = bestRight + 1; i < n; i++) {
-      cost += arr[i] - arr[bestRight];
-    }
-
-    bw.write(String.valueOf(cost));
-    bw.newLine();
-
-    bw.flush();
-    bw.close();
-    br.close();
-  }
 }
