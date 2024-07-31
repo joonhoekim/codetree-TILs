94ef71865935f10acc8097add88f57115cd15b1e
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-07-31 19:15:13 +0900

    Title: 구역 청소, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

--- 240731/구역 청소/district-cleaning.java
+++ 240731/구역 청소/district-cleaning.java
@@ -10,31 +10,19 @@ public class Main {
      * 설계
      * - 겹치는 부분을 빼 주는 방법
      * - 구역으로 설정된 경우를 구역을 표현한 자료구조에 기록하는 방법 (배열 등)
+     *   - 이 방법은 구역의 크기가 커지는 경우 불리해진다.
      * */
 
     Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-    int c = sc.nextInt();
-    int d = sc.nextInt();
+    int aMin = sc.nextInt();
+    int aMax = sc.nextInt();
+    int bMin = sc.nextInt();
+    int bMax = sc.nextInt();
 
-    int arr[] = new int[101];
-
-    for (int i = a; i < b; i++) {
-      arr[i]++;
+    if (aMax < bMin || aMin > bMax) {
+      System.out.println(aMax - aMin + bMax - bMin);
+    } else {
+      System.out.println(Math.max(aMax, bMax) - Math.min(aMin, bMin));
     }
-
-    for (int i = c; i < d; i++) {
-      arr[i]++;
-    }
-
-    int ans = 0;
-    for (int i = 0; i <= 100; i++) {
-      if (arr[i] != 0) {
-        ans++;
-      }
-    }
-
-    System.out.println(ans);
   }
 }
