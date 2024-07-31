1c851bf361306c779158350ab47de172a6e0d0b0
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-07-31 21:09:59 +0900

    Title: 코딩톡, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240731/코딩톡/coding-talk.java
+++ 240731/코딩톡/coding-talk.java
@@ -28,7 +28,19 @@ public class Main {
      * - N번째 메세지 이후, 메세지를 발송한 사람은 '코딩톡 들어온 사람'이므로 확실히 읽은 사람
      * - N번째 메세지 발송자는 확실히 읽은 사람
      * - '읽지 않은 사람의 수'?... 그것을 알아도 누군가의 '읽지 않았을 가능성'을 완전히 지울 수 없지 않나?
+     *   - 아 이전에 풀었던 '밀어내기'랑 유사한건가?
      * */
+
+    /*
+    이 테스트케이스를 보면, 1번째 메세지 안읽은 사람이 0이다. 이런 경우 전부 읽었다는 것이 성립한다.
+    6 6 1
+    D 0
+    C 1
+    B 2
+    B 2
+    A 2
+    F 4
+    * */
 
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
@@ -41,6 +53,10 @@ public class Main {
       //센더 ID를 int로 저장
       senders[i] = (sc.next().charAt(0) - 'A');
       unreadCount[i] = sc.nextInt();
+    }
+
+    if (unreadCount[p - 1] == 0) {
+      return;
     }
 
     boolean[] possibleUnreadUserIds = new boolean[n];
