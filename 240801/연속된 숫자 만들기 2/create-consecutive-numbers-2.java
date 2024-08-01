dea7f549ac1e08e0471facabdebe7c8e7ae8a4bf
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-08-01 17:12:28 +0900

    Title: 연속된 숫자 만들기 2, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240801/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
+++ 240801/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -15,15 +15,22 @@ public class Main {
     // 두 사람 사이로 이동할 때 한 칸을 비워놔야 완성됨
 
     // 케이스
+    // 1 2 3
+    // 1 2 4 -> 2 3 4
+    // 1 2 5 -> 2 4 5 -> 2 3 4
+    // 1 3 4 -> 1 2 3
     // 1 3 5 -> 1 2 3
     // 1 2 10 -> 2 4 10 -> 2 3 4
+    // 1 8 10 ->
     // 1 9 10 -> 1 3 9 -> 1 2 3
+    // 2 4 6 -> 2 3 4
 
     // 가까운 숫자간 차이를 구하는데,
-    // 1 차이나는 것이
-    //   - 0개 있으면 2회
-    //   - 1개 있으면 1회
-    //   - 2개 있으면 0회
+    // 1. 공차 1 = 1개, 공차 2 = 0개 -> 이동횟수 2
+    // 2. 공차 1 = 2개 -> 이동횟수 0 (이미 정렬됨)
+    // 3. 공차 1 = 1개, 공차 2 = 1개 -> 이동횟수 1 (사이로 들어가면 됨)
+    // 4. 공차 1 = 0개, 공차 2 = 1개 -> 이동횟수 1 (마찬가지로 사이로 들어가면 됨)
+    // 5. 공차 1 = 0 개, 공차 2 = 2개 -> 이동횟수 1 (양쪽 아무거나 사이로 들어가면 됨)
 
     Scanner sc = new Scanner(System.in);
     int n = 3;
@@ -35,14 +42,33 @@ public class Main {
 
     Arrays.sort(arr);
     // 공차 1인 경우 개수찾기
-    int count = 0;
+    int countDistOne = 0;
+    int countDistTwo = 0;
     for (int i = 0; i < n - 1; i++) {
       if (arr[i + 1] - arr[i] == 1) {
-        count++;
+        countDistOne++;
+      } else if (arr[i + 1] - arr[i] == 2) {
+        countDistTwo++;
       }
     }
 
-    System.out.println(2 - count);
+    if (countDistOne == 1 && countDistTwo == 0) {
+      System.out.println("2");
+    }
+
+    if (countDistOne == 2) {
+      System.out.println("0");
+    }
+
+    if (countDistTwo == 1) {
+      System.out.println("1");
+    }
+    
+    if (countDistOne == 0 && countDistTwo == 0) {
+      System.out.println("2");
+    }
+    
+    
 
 
   }
