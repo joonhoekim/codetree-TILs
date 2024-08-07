ed2e8f09ab5896d5b0e4d7991c89f2e40753288b
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-08-07 19:32:29 +0900

    Title: 세 수의 최대 곱, Time: 287ms, Memory: 19MB, Status: Passed - CodeTree

--- 240807/세 수의 최대 곱/maximum-product-of-three-numbers.java
+++ 240807/세 수의 최대 곱/maximum-product-of-three-numbers.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,13 +13,19 @@ public class Main {
      * - 3개 곱해서 나올 수 있는 최댓값 구하기
      * 분석
      * - 수가 1000개 주어진다.
-     * - 완전탐색하는 경우 1000^3 = 10^9 < 21.4억 미만임
+     * - 완전탐색하는 경우 1000^3 = 10^9 = 10억 정도로, 1초안에 해결 불가
      * 설계
      * - 일단 완전탐색으로 풀어보자(아마 시간초과?)
      * - 음수 * 음수 * 양수 혹은 양수 * 양수 * 양수
      * - 즉 짝수개의 음수를 곱하는 게 전략일 것이다.
      * 구현
      * - 일단 무식하게 구해보자
+     * >> 역시 시간 초과다.
+     *
+     * 전략
+     * - 1000개를 정렬하는데 얼마 걸리지 않을 것임 (nlogn)
+     * - 정렬하고 두가지 케이스 비교하면 된다.
+     *   - ++-, --+
      * */
 
     public static void main(String[] args) throws Exception {
@@ -32,16 +39,12 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int globalMax = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    globalMax = Math.max(globalMax,
-                                         arr[i] * arr[j] * arr[k]);
-                }
-            }
-        }
+        Arrays.sort(arr);
+        int mmp = arr[0] * arr[1] * arr[arr.length-1];
+        int ppp = arr[arr.length - 1] * arr[arr.length - 2] * arr[arr.length - 3];
 
+        int globalMax = Math.max(mmp,
+                                 ppp);
         System.out.println(globalMax);
 
     }
