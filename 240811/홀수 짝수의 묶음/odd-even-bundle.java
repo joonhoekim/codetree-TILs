b5015ee65920cc9bc27e64115cd6fe73addb2393
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-08-11 20:53:01 +0900

    Title: 홀수 짝수의 묶음, Time: 81ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240811/홀수 짝수의 묶음/odd-even-bundle.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        int oddCount = 0;
+        int evenCount = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (arr[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        bw.write(String.valueOf(solve(oddCount,
+                                      evenCount)));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    /*
+     * 문제 조건을 잘못 이해함
+     * 부분합이 원소 한개여도 가능함
+     *
+     * 그럼 짝홀짝홀짝홀 1개 원소로 만들되
+     * - 짝수는 몇개가 남아도 상관 없고
+     * - 홀수는 홀+홀 = 짝 으로 바꿔서 써야 할 것임
+     *
+     * - 짝수가 더 많으면, 홀수개수 * 2 가 답이 될 거고
+     * - 홀수가 더 많으면, 홀수를 더해서 짝수로 바꿔주면 됨
+     * */
+
+    static int solve(int oddCount, int evenCount) {
+
+        if (evenCount >= oddCount) {
+            //짝수가 더 많으면, 홀수 개수에 의존적임
+            return oddCount * 2 + 1;
+        } else if (evenCount < oddCount) {
+            // 홀수 2x 개를 x개의 짝수로 바꾼다.
+            // evencount + x = oddCount - 2x
+            // 3x = oddCount - evenCount (단 x>=1 이어야 함 )
+            // 올림 처리해서, 짝홀짝홀 유지 가능하도록 함
+            // 이렇게 처리하면, 최대 길이는 짝수 개수에 의존적임.
+            // 단 홀수가 홀수개여야 함! 안그러면 짝수로 변환하고, 홀수자리를 완성 불가
+
+            int x = (oddCount - evenCount + 3) / 3;
+            oddCount -= 2 * x;
+            evenCount += x;
+
+        }
+
+        return solve(oddCount,
+                     evenCount);
+
+    }
+}
