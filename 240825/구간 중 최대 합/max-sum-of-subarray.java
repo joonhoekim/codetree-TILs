d22b836a9f5790dba299051f2f6136986a47462d
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-08-25 21:25:48 +0900

    Title: 구간 중 최대 합, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240825/구간 중 최대 합/max-sum-of-subarray.java
+++ 240825/구간 중 최대 합/max-sum-of-subarray.java
@@ -15,7 +15,7 @@ public class Main {
     }
 
     int max = Integer.MIN_VALUE;
-    for (int i = 0; i < n-k; i++) {
+    for (int i = 0; i <= n-k; i++) {
       int sum = 0;
       for (int j = i; j < i+k; j++) {
         sum += arr[j];
