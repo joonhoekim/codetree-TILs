e73cdd23f90081b4b856c34fcd1ac82358da2655
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-08-26 17:21:25 +0900

    Title: 원형 순열에서의 인원 제거, Time: 389ms, Memory: 78MB, Status: Passed - CodeTree

--- 240826/원형 순열에서의 인원 제거/josephus-permutations.java
+++ 240826/원형 순열에서의 인원 제거/josephus-permutations.java
@@ -1,36 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
 
-  /*
-   * Josephus 이야기
-   * 원형으로 둘러앉고 N 번째사람을 제거
-   *
-   * */
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int k = sc.nextInt();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
 
-    //사람 번호를 큐에 넣는다.
-    Queue<Integer> queue = new LinkedList<>();
-    for (int i = 1; i <= n; i++) {
-      queue.add(i);
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < k - 1; i++) {
+                queue.offer(queue.poll());
+            }
+            bw.write(queue.poll() + "");
+            if (!queue.isEmpty()) {
+                bw.write(" ");
+            }
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
-
-    while (queue.size() > 0) {
-      //K번째 사람을 찾기 위해 큐를 순환시키고, 제거한다.
-      int dead = 0;
-      for (int i = 0; i < k - 1; i++) {
-        queue.add(queue.poll());
-      }
-      int removed = queue.poll();
-      System.out.print(removed + " ");
-      //이것을 모두 제거하기까지 반복한다.
-    }
-
-
-  }
 }
