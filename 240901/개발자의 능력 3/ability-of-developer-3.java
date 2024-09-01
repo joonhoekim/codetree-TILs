81f322e9c84449b4b5402392e3339b750811b282
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-01 13:48:55 +0900

    Title: 개발자의 능력 3, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240901/개발자의 능력 3/ability-of-developer-3.java
+++ 240901/개발자의 능력 3/ability-of-developer-3.java
@@ -5,19 +5,19 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    int[] devolopers = new int[6];
+    int[] developers = new int[6];
     int total = 0;
     for (int i = 0; i < 6; i++) {
-      devolopers[i] = sc.nextInt();
-      total += devolopers[i];
+      developers[i] = sc.nextInt();
+      total += developers[i];
     }
 
     int min = Integer.MAX_VALUE;
 
-    for (int i = 0; i < 4; i++) {
-      for (int j = 1; j < 5; j++) {
-        for (int k = 2; k < 6; k++) {
-          int group1 = devolopers[i] + devolopers[j] + devolopers[k];
+    for (int i = 0; i < 6; i++) {
+      for (int j = i+1; j < 6; j++) {
+        for (int k = j+1; k < 6; k++) {
+          int group1 = developers[i] + developers[j] + developers[k];
           int group2 = total - group1;
           min = Math.min(min, Math.abs(group1-group2));
         }
