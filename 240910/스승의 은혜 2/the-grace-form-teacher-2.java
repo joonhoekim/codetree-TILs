d8a0ad0e32ba4da9c70db449f077212960a61f39
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-10 22:29:00 +0900

    Title: 스승의 은혜 2, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240910/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+  import java.util.Scanner;
+
+  public class Main {
+
+    public static int[] P = new int[1000];
+
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      int N = sc.nextInt();
+      int B = sc.nextInt();
+
+      for (int i = 0; i < N; i++) {
+        P[i] = sc.nextInt();
+      }
+
+      int max = 0;
+
+      for (int i = 0; i < N; i++) {
+        int sum = 0;
+        int count = 0;
+        for (int j = 0; j < N; j++) {
+          int price = P[j];
+          if (sum + price > B) {
+            continue;
+          }
+
+          if (i==j) {
+            price /= 2;
+          }
+          sum += price;
+          count++;
+        }
+
+        max = Math.max(max, count);
+      }
+
+      System.out.println(max);
+    }
+  }
