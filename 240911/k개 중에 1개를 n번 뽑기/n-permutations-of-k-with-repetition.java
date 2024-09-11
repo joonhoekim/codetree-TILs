b11ff787a4f2a1682037ab5558956c5a68261cf0
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-11 21:47:57 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 291ms, Memory: 21MB, Status: Passed - CodeTree

--- 240911/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
+++ 240911/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -1,45 +1,44 @@
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
-    private static int[] answer;
-    private static int maxValue;
-    private static int maxDigit;
-    private static BufferedWriter bw;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static List<Integer> answer = new LinkedList<>();
+    static int maxValue;
+    static int maxDigit;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String[] input = br.readLine().split(" ");
-        maxValue = Integer.parseInt(input[0]);
-        maxDigit = Integer.parseInt(input[1]);
-
-        answer = new int[maxDigit];
-        
-        choose(0);
-
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    private static void choose(int currDigit) throws IOException {
-        if (currDigit == maxDigit) {
-            writeResult();
+    static void choose(int currDigit) throws IOException {
+        if (currDigit == 0) {
+            print();
             return;
         }
 
         for (int i = 1; i <= maxValue; i++) {
-            answer[currDigit] = i;
-            choose(currDigit + 1);
+            answer.add(i);
+            choose(currDigit - 1);
+            answer.remove(answer.size() - 1);
         }
     }
 
-    private static void writeResult() throws IOException {
-        for (int i = 0; i < maxDigit; i++) {
-            bw.write(Integer.toString(answer[i]));
-            bw.write(' ');
+    static void print() throws IOException {
+        for (Integer integer : answer) {
+            bw.write(integer.toString());
+            bw.write(" ");
         }
         bw.newLine();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String[] input = br.readLine().split(" ");
+        maxValue = Integer.parseInt(input[0]);
+        maxDigit = Integer.parseInt(input[1]);
+
+        choose(maxDigit);
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
