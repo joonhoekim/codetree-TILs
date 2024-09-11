2b5373db996b2b3422d9ae60c1d098711f035601
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-11 21:44:25 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 849ms, Memory: 37MB, Status: Passed - CodeTree

--- 240911/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
+++ 240911/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.java
@@ -1,45 +1,48 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-  static Scanner sc = new Scanner(System.in);
-  static List<Integer> answer = new LinkedList<Integer>();
-  static int maxValue;
-  static int maxDigit;
+    static Scanner sc = new Scanner(System.in);
+    static List<Integer> answer = new LinkedList<>();
+    static int maxValue;
+    static int maxDigit;
+    static List<String> results = new LinkedList<>(); // 결과를 저장할 리스트
 
-  static void choose(int currNum) {
-    if(currNum==0) {
-      print();
-      return;
+    static void choose(int currDigit) {
+        if (currDigit == 0) {
+            saveResult();
+            return;
+        }
+
+        for (int i = 1; i <= maxValue; i++) {
+            answer.add(i);
+            choose(currDigit - 1);
+            answer.remove(answer.size() - 1);
+        }
     }
 
-    for (int i = 1; i <= maxValue; i++) {
-      answer.add(i);
-      choose(currNum-1);
-      answer.remove(answer.size()-1);
+    static void saveResult() {
+        StringBuilder sb = new StringBuilder();
+        for (Integer integer : answer) {
+            sb.append(integer).append(" ");
+        }
+        results.add(sb.toString());
     }
-    
-  }
 
-  static void print() {
-    StringBuilder sb = new StringBuilder();
-    for (Integer integer : answer) {
-      sb.append(integer).append(" ");
+    static void printResults() {
+        for (String result : results) {
+            System.out.println(result);
+        }
     }
-    System.out.println(sb);
-  }
 
-  public static void main(String[] args) {
-    int k = sc.nextInt();
-    int n = sc.nextInt();
-    maxValue = k;
-    maxDigit = n;
+    public static void main(String[] args) {
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        maxValue = k;
+        maxDigit = n;
 
-    choose(maxDigit);
-
-
-
-  }
+        choose(maxDigit);
+        printResults(); // 모든 결과를 한 번에 출력
+    }
 }
