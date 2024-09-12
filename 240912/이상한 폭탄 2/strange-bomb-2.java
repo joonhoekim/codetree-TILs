f23a184c08610eec73197562067c9b4e6f151ee9
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-12 23:18:21 +0900

    Title: 이상한 폭탄 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240912/이상한 폭탄 2/strange-bomb-2.java
+++ 240912/이상한 폭탄 2/strange-bomb-2.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
 
-  public class Main {
-    public static int[] box = new int[100];
+public class Main {
+  public static int[] box = new int[100];
 
-    public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-      int N = sc.nextInt();
-      int K = sc.nextInt();
+    int N = sc.nextInt();
+    int K = sc.nextInt();
 
-      for (int i = 0; i < N; i++) {
-        box[i] = sc.nextInt();
-      }
+    for (int i = 0; i < N; i++) {
+      box[i] = sc.nextInt();
+    }
 
-      int bombNo = -1;
+    int bombNo = -1;
 
-      for (int i = 0; i < N; i++) {
-        for (int j = i+1; j <= i+K; j++) {
-          if (box[i] == box[j]) {
-            bombNo = box[j];
-          }
+    for (int i = 0; i < N; i++) {
+      for (int j = i+1; j <= i+K; j++) {
+        if (box[i] == box[j]) {
+          bombNo = Math.max(bombNo, box[j]);
         }
       }
-
-      System.out.println(bombNo);
     }
+
+
+    // 결과 출력
+    System.out.println(bombNo);
   }
+}
