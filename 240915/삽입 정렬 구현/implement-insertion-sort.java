f9bc048d25bedb7a17c8158f9c1427b1dbeb1c21
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-15 20:32:34 +0900

    Title: 삽입 정렬 구현, Time: 278ms, Memory: 15MB, Status: Passed - CodeTree

--- 240915/삽입 정렬 구현/implement-insertion-sort.java
+++ 240915/삽입 정렬 구현/implement-insertion-sort.java
@@ -26,14 +26,20 @@ public class Main {
     }
 
     static void insertionSort(int[] arr) {
+        //i는 정렬되지 않은 부분의 시작 idx이다.
         for (int i = 1; i < arr.length; i++) {
+            //key는 삽입할 대상이다. 여기서는 정렬되지 않은 부분의 시작점이다.
             int key = arr[i];
+            //j는 정렬된 부분의 마지막 idx이다.
             int j = i - 1;
 
+            //만약 key보다 크면, key가 더 안쪽으로 들어가야 하니까 밀어내는 과정이다.
+            //'밀어내는 방식'이기 때문에 key만 기억하고 있으면 된다.
             while (j >= 0 && arr[j] > key) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
             }
+            //공간을 다 만들었으면, key를 넣어준다.
             arr[j + 1] = key;
         }
     }
