102e1daf0c363e13cc56d52431b868f1315292fb
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-17 19:37:53 +0900

    Title: 정보에 따른 숫자 2, Time: 1958ms, Memory: 10MB, Status: Passed - CodeTree

--- 240917/정보에 따른 숫자 2/number-based-on-information-2.java
+++ 240917/정보에 따른 숫자 2/number-based-on-information-2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static char[] arr = new char[1000];
+  public static char[] arr = new char[1001];
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
@@ -21,12 +21,13 @@ public class Main {
     for (int k = a; k <= b; k++) {
       int d1 = Integer.MAX_VALUE;
       int d2 = Integer.MAX_VALUE;
-      for (int i = 0; i < 1000; i++) {
-        for (int j = 0; j < 1000; j++) {
+      for (int i = 0; i < 1001; i++) {
+        for (int j = 0; j < 1001; j++) {
           if (arr[i] == 'S' && arr[j] == 'N') {
             d1 = Math.min(d1, Math.abs(k-i));
             d2 = Math.min(d2, Math.abs(k-j));
           }
+
         }
       }
       if(d1 <= d2) {
