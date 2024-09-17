cef1dda14bfb65278cbf3ed525f3ad42ad0fcf3b
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-17 19:48:26 +0900

    Title: 기수 정렬 구현, Time: 654ms, Memory: 33MB, Status: Passed - CodeTree

--- 240917/기수 정렬 구현/implement-radix-sort.java
+++ 240917/기수 정렬 구현/implement-radix-sort.java
@@ -1,96 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    br.readLine();
+    br.readLine(); // 첫 번째 줄은 배열의 크기이므로 무시
     int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-//    int k = getDigitLength(getMax(arr));
+    int k = getDigitLength(getMax(arr));
 
-    int[] sortedArr = radixSort(arr, 6);
+    radixSort(arr, k);
 
-    String answer = String.join(" ", Arrays.stream(sortedArr).mapToObj(String::valueOf).toArray(String[]::new));
-    
+    String answer = String.join(" ", Arrays.stream(arr).mapToObj(String::valueOf).toArray(String[]::new));
+
     System.out.println(answer);
   }
 
   private static int getDigitLength(int num) {
-    int digit = 0;
-    while (num > 0) {
-      num = num / 10;
-      digit++;
-    }
-    return digit;
+    if (num == 0) return 1;  // 0의 자릿수는 1로 처리
+    return (int) Math.log10(Math.abs((double) num)) + 1;
   }
-
-
 
   private static int getMax(int[] arr) {
-    int max = Integer.MIN_VALUE;
-    for (int i = 0; i < arr.length; i++) {
-      max = Math.max(max, arr[i]);
+    return Arrays.stream(arr).max().orElse(Integer.MIN_VALUE);
+  }
+
+  private static void radixSort(int[] arr, int k) {
+    final int BASE = 10;
+    int[] output = new int[arr.length];
+
+    for (int exp = 1; k > 0; exp *= 10, k--) {
+      int[] count = new int[BASE];
+
+      // Count occurrences
+      for (int value : arr) {
+        count[(value / exp) % 10]++;
+      }
+
+      // Calculate cumulative count
+      for (int i = 1; i < BASE; i++) {
+        count[i] += count[i - 1];
+      }
+
+      // Build output array
+      for (int i = arr.length - 1; i >= 0; i--) {
+        int index = (arr[i] / exp) % 10;
+        output[count[index] - 1] = arr[i];
+        count[index]--;
+      }
+
+      // Copy output to arr
+      System.arraycopy(output, 0, arr, 0, arr.length);
     }
-    return max;
   }
-
-
-  /*
-  *
-function radix_sort(arr, k)
-  for pos = k - 1 ... pos >= 0:
-    set arr_new = [10][]
-    for i = 0 ... i < arr.size
-      set digit = posth digit of arr[i]
-      arr_new[digit].append(arr[i])
-
-    set store_arr = []
-    for i = 0 ... i < 10
-      for j = 0 ... j < arr_new[i].size
-        store_arr.append(arr_new[i][j])
-
-    arr = store_arr
-
-  return arr
-
-  * */
-  public static int[] radixSort(int[] arr, int k) {
-    for (int pos = k - 1; pos >= 0; pos--) {
-      List<List<Integer>> arrNew = new ArrayList<>(10);
-      for (int i = 0; i < 10; i++) {
-        arrNew.add(new ArrayList<>());
-      }
-
-      // 현재 자릿수에 따라 숫자들을 분류
-      for (int num : arr) {
-        int digit = getDigit(num, pos);
-        arrNew.get(digit).add(num);
-      }
-
-      // 분류된 숫자들을 순서대로 새 배열에 저장
-      int[] storeArr = new int[arr.length];
-      int index = 0;
-      for (List<Integer> bucket : arrNew) {
-        for (int num : bucket) {
-          storeArr[index++] = num;
-        }
-      }
-
-      // 원본 배열을 새로 정렬된 배열로 교체
-      arr = storeArr;
-    }
-
-    return arr;
-  }
-
-  private static int getDigit(int num, int pos) {
-    return (num / (int) Math.pow(10, pos)) % 10;
-  }
-
-
 }
