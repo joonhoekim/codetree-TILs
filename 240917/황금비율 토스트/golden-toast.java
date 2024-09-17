400cee8247c6b8af46858018cc7df02941dde3b9
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-17 20:54:37 +0900

    Title: 황금비율 토스트, Time: 537ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240917/황금비율 토스트/golden-toast.java
+++ 240917/황금비율 토스트/golden-toast.java
@@ -1,48 +1,57 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
 public class Main {
 
-  public static void main(String[] args) throws Exception {
-    try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
-      //command[0] = 식빵의 개수, command[1] = 레시피 암호문의 개수
-      int[] firstlineArgs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-      String secondlineInput = br.readLine();
-      List<Character> list = new LinkedList<>();
-      for (int i = 0; i < secondlineInput.length(); i++) {
-        list.add(secondlineInput.charAt(i));
-      }
-      ListIterator<Character> iterator = list.listIterator(list.size());
+            // command[0] = 식빵의 개수, command[1] = 레시피 암호문의 개수
+            String[] firstLine = br.readLine().split(" ");
+            int commandCount = Integer.parseInt(firstLine[1]);
+            
+            String secondlineInput = br.readLine();
+            List<Character> list = new LinkedList<>();
+            for (char c : secondlineInput.toCharArray()) {
+                list.add(c);
+            }
+            ListIterator<Character> iterator = list.listIterator(list.size());
 
-      //명령 받기
-      for (int i = 0; i < firstlineArgs[1]; i++) {
-        String[] command = br.readLine().split(" ");
-        try{
-          switch (command[0]) {
-            case "L" : iterator.previous(); break;
-            case "R" : iterator.next(); break;
-            case "D" : {if(iterator.hasNext()) {iterator.next();} iterator.remove();} break;
-            case "P" : iterator.add(command[1].charAt(0)); break;
-//            case "L" -> System.out.println("R: " + iterator.previous());
-//            case "R" -> System.out.println("R: " + iterator.next());
-//            case "D" -> {iterator.next(); iterator.remove();}
-//            case "P" -> {iterator.add(command[1].charAt(0));
-//              System.out.println("added " + command[1]);}
-          }
-        } catch (Exception e){
-          System.out.println(e.getMessage());
+            // 명령 받기
+            for (int i = 0; i < commandCount; i++) {
+                String[] command = br.readLine().split(" ");
+                try {
+                    switch (command[0]) {
+                        case "L":
+                            if (iterator.hasPrevious()) iterator.previous();
+                            break;
+                        case "R":
+                            if (iterator.hasNext()) iterator.next();
+                            break;
+                        case "D":
+                            if (iterator.hasNext()) {
+                                iterator.next();
+                            }
+                            iterator.remove();
+                            break;
+                        case "P":
+                            iterator.add(command[1].charAt(0));
+                            break;
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+
+            // 최종 리스트 출력
+            StringBuilder sb = new StringBuilder();
+            for (Character c : list) {
+                sb.append(c);
+            }
+            System.out.println(sb.toString());
         }
-      }
-
-      // 최종 리스트 출력
-      StringBuilder sb = new StringBuilder();
-      list.iterator().forEachRemaining(sb::append);
-      System.out.println(sb.toString());
     }
-  }
 }
