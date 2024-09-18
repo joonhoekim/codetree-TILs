377389ee82252b00a220afb35af18b7291be9f17
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-18 21:03:51 +0900

    Title: 등차수열, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240918/등차수열/arithmetic-sequence.java
+++ 240918/등차수열/arithmetic-sequence.java
@@ -16,7 +16,7 @@ public class Main {
 
       int count = 0;
       for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
+        for (int j = i+1; j <= n; j++) {
           for (int k = 1; k <= n; k++) {
             if (a[j] - k == k - a[i]) {
               count++;
