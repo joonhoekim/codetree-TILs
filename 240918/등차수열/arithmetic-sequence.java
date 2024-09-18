08105b4c4c2a506432ca10ddc8654fe9aea0eca1
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-18 21:00:36 +0900

    Title: 등차수열, Time: 110ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240918/등차수열/arithmetic-sequence.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+  public static int[] a = new int[101];
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    for (int i = 1; i <= n; i++) {
+      a[i] = sc.nextInt();
+    }
+
+    int max = Integer.MIN_VALUE;
+
+      int count = 0;
+      for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+          for (int k = 1; k <= n; k++) {
+            if (a[j] - k == k - a[i]) {
+              count++;
+            }
+        }
+      }
+      max = Math.max(max, count);
+    }
+
+    System.out.println(max);
+  }
+}
