7110cd43c51c6ad62f997b960c2db5d1db981496
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-09-28 18:24:39 +0900

    Title: 가장 작은 x 찾기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240928/가장 작은 x 찾기/find-smallest-x.java
+++ 240928/가장 작은 x 찾기/find-smallest-x.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
   public static int MAX_N = 10;
+  public static int MAX_AB = 10000;
   public static int[] a = new int[MAX_N];
   public static int[] b = new int[MAX_N];
 
@@ -18,8 +19,10 @@ public class Main {
 
     int min = Integer.MAX_VALUE;
 
-    // x가 모든 조건은 만족해야 하므로 첫번째 ai 와 bi 조건에 맞는 범위를 x로 완전탐색
-    for (int i = a[0]; i <= b[0]; i++) {
+    // 가능한 x의 모든 범위 완전탐색
+    // min값이 갱신되면 반복문이 종료되도록 구현함.
+    // -> x가 작은값부터 순서대로 탐색하기 때문
+    for (int i = 1; i <= MAX_AB; i++) {
       boolean isMeet = true;
       int x = i; // x를 별도로 지정(2곱하기를 반복하기 위해)
 
@@ -34,7 +37,6 @@ public class Main {
       }
 
       // 조건을 충족하는 경우 정답 갱신
-      // x가 작은 값부터 시작하기 때문에
       if (isMeet) {
         min = i;
         break;
