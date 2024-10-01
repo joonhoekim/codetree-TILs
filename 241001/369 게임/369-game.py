c0871df2f33949375b2beafa64adecebdb379bc0
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-10-01 15:29:41 +0900

    Title: 369 게임, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 241001/369 게임/369-game.py
+++ 241001/369 게임/369-game.py
@@ -4,15 +4,15 @@ for i in range(1, n+1):
     if cond1:
         print(0, end=" ")
         continue
-    temp = i
-
+    
     cond2=False
+    temp = i
     while temp>0:
         last_digit = temp%10
-        if(last_digit%3==0):
+        if(last_digit!=0 and last_digit%3==0):
             cond2=True
             break
-        temp /= 10
+        temp = temp//10
         
     if cond2:
         print(0, end=" ")
