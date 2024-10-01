afa8d1c91f3dba9e43fb888e5859a6580ea936d0
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-10-01 15:24:11 +0900

    Title: 369 게임, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241001/369 게임/369-game.py
@@ -0,0 +1,21 @@
+n=int(input())
+for i in range(1, n+1):
+    cond1 = i%3==0
+    if cond1:
+        print(0, end=" ")
+        break;
+    temp = i
+
+    cond2=False
+    while temp>0:
+        last_digit = temp%10
+        if(last_digit%3==0):
+            cond2=True
+            break
+        temp /= 10
+        
+    if cond2:
+        print(0, end=" ")
+        break;
+    
+    print(i, end=" ")
