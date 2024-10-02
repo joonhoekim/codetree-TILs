897da030ebc889fa32fd58dc7e5da49e947ad8f1
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-10-02 17:02:28 +0900

    Title: 대문자로 바꾸기, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 241002/대문자로 바꾸기/change-to-capital.py
+++ 241002/대문자로 바꾸기/change-to-capital.py
@@ -1,6 +1,18 @@
+# 내장함수들을 이용한 간결한 풀이
 row_count=5
-column_count=3
-arr_2d=[]
+# for i in range(row_count):
+#     arr_2d=list(input().upper().split())
+#     print(' '.join(arr_2d))
+
+# 다른 언어에도 적용되는, 문자열의 정수값을 비교해 푸는 풀이
+arr_2d = [
+    list(input().split())
+    for i in range(row_count)
+]
+
+# 하나씩 처리하는 방식
 for i in range(row_count):
-    arr_2d=list(input().upper().split())
-    print(' '.join(arr_2d))
+    for j in range(len(arr_2d[i])):
+        arr_2d[i][j] = chr(ord(arr_2d[i][j]) + ord('A') - ord('a'))
+        print(arr_2d[i][j], end=' ')
+    print()
