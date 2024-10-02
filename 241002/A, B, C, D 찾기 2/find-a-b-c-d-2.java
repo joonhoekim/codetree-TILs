9921b7f01dc8ebd29c9d0a94aefcfb27b3abcbe6
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-10-02 16:17:50 +0900

    Title: A, B, C, D 찾기 2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241002/A, B, C, D 찾기 2/find-a-b-c-d-2.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+  public static int N = 15;
+  public static ArrayList<Integer> list = new ArrayList<>();
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    // 입력
+    for (int i = 0; i < N; i++) {
+      list.add(sc.nextInt());
+    }
+
+    Collections.sort(list);
+
+    loop:
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        for (int k = 0; k < N; k++) {
+          for (int l = 0; l < N; l++) {
+            int a = list.get(i);
+            int b = list.get(j);
+            int c = list.get(k);
+            int d = list.get(l);
+            if(isContain(a, b, c, d)) {
+              System.out.printf("%d %d %d %d\n",a, b, c, d);
+              break loop;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  public static boolean isContain(int A, int B, int C, int D) {
+    if (list.contains(A+B) &&
+        list.contains(B+C) &&
+        list.contains(C+D) &&
+        list.contains(D+A) &&
+        list.contains(A+C) &&
+        list.contains(B+D) &&
+        list.contains(A+B+C) &&
+        list.contains(A+B+D) &&
+        list.contains(A+C+D) &&
+        list.contains(B+C+D) &&
+        list.contains(A+B+C+D)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
