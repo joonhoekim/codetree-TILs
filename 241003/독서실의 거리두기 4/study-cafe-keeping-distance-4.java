47f79b7769150153ac048898fea9f67cd03bf5a4
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-10-03 23:08:14 +0900

    Title: 독서실의 거리두기 4, Time: 142ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241003/독서실의 거리두기 4/study-cafe-keeping-distance-4.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    sc.nextLine();
+    char[] seats = sc.nextLine().toCharArray();
+
+    int answer = Integer.MIN_VALUE;
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (i == j || seats[i] == '1' || seats[j] == '1') {
+          continue;
+        }
+
+        char[] newSeats = Arrays.copyOf(seats, N);
+        newSeats[i] = '1';
+        newSeats[j] = '1';
+
+        int closest = Integer.MAX_VALUE;
+        int distance = 0;
+        boolean isCounting = false;
+
+        for (int k = 0; k < N; k++) {
+          if (newSeats[k] == '1') {
+            if (isCounting) {
+              closest = Math.min(distance, closest);
+            }
+            distance = 0;
+            isCounting = true;
+          }
+
+          distance++;
+        }
+
+        answer = Math.max(closest, answer);
+      }
+    }
+    System.out.println(answer);
+  }
+}
