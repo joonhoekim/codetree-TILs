11a66e3ffc90253837546688e8ab17b8384e36bf
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-10-04 17:56:45 +0900

    Title: 이상한 폭탄 3, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 241004/이상한 폭탄 3/strange-bomb-3.java
+++ 241004/이상한 폭탄 3/strange-bomb-3.java
@@ -20,37 +20,40 @@ public class Main {
     for (int i = 0; i < N; i++) {
       bombs[i] = sc.nextInt();
     }
-    int maxNo = 0;
+
     for (int i = 0; i < N; i++) {
-      for (int j = i+1; j <= i+K; j++) {
-        if (i == j) {
-          continue;
-        }
+      for (int j = i + 1; j <= i + K && j < N; j++) {  
         if (bombs[i] == bombs[j]) {
           if (!isExploded[i]) {
             countMap.put(bombs[i], countMap.getOrDefault(bombs[i], 0) + 1);
+            isExploded[i] = true;  
           }
           if (!isExploded[j]) {
             countMap.put(bombs[j], countMap.getOrDefault(bombs[j], 0) + 1);
+            isExploded[j] = true;  
           }
-
-          isExploded[i] = true;
-          isExploded[j] = true;
         }
       }
     }
 
     int maxCount = 0;
+    int maxNo = 0;
+
     for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
       if (entry.getValue() > maxCount) {
         maxCount = entry.getValue();
         maxNo = entry.getKey();
+      } else if (entry.getValue() == maxCount) {
+        maxNo = Math.max(maxNo, entry.getKey());  
       }
     }
+
     if (maxCount == 0) {
       System.out.println(0);
     } else {
       System.out.println(maxNo);
     }
+
+    sc.close();
   }
 }
