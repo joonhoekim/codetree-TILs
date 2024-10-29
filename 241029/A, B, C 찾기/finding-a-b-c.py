2805c019ac53b50cf33b5ace3f75c759407a07d5
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-10-29 17:19:04 +0900

    Title: A, B, C 찾기, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 241029/A, B, C 찾기/finding-a-b-c.py
+++ 241029/A, B, C 찾기/finding-a-b-c.py
@@ -1,13 +1,14 @@
 arr = list(map(int, input().split()))
-
-arr.sort()
-
 n = len(arr)
+arr.sort()
 A, B, C = 0, 0, 0
 
 for i in range(n-2):
   for j in range(1, n-1):
     for k in range(2, n):
+      if i == j or j == k or i == k:
+        continue
+      
       if arr[i] + arr[j] + arr[k] == arr[n-1]:
         A = arr[i]
         B = arr[j]
