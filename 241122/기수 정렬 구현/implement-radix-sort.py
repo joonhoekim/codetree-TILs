f0c8231ceeca31876e1f3e269e4df1aff3e68920
Joonhoe Kim <34194400+joonhoekim@users.noreply.github.com>
2024-11-22 22:46:55 +0900

    Title: 기수 정렬 구현, Time: 98ms, Memory: 26MB, Status: Passed - CodeTree

--- 241122/기수 정렬 구현/implement-radix-sort.py
+++ 241122/기수 정렬 구현/implement-radix-sort.py
@@ -1,21 +1,3 @@
-# function radix_sort(arr, k)
-#   # pos는 현재 보고있는 자리 수를 의미
-#   for pos = k - 1 ... pos >= 0:
-#     # 0부터 9까지 정렬결과를 담을 이차원 배열
-#     set arr_new = [10][]
-#     for i = 0 ... i < arr.size
-#       set digit = posth digit of arr[i]
-#       arr_new[digit].append(arr[i])
-
-#     # 0부터 9까지 순서대로 안에 있는 값을 담아 arr로 옮긴다.
-#     set store_arr = []
-#     for i = 0 ... i < 10
-#       for j = 0 ... j < arr_new[i].size
-#         store_arr.append(arr_new[i][j])
-  
-#     arr = store_arr
-
-#   return arr
 MAX_K = 6
 MAX_DIGIT = 10
 
@@ -24,17 +6,17 @@ arr = list(map(int, input().split()))
 
 p = 1
 for pos in range(MAX_K):
-  new_arr = [[] for _ in range(MAX_DIGIT)]
-  for elem in arr:
-    digit = (elem // p) % 10
-    new_arr[digit].append(elem)
-
-  arr = []
-  for digit in range(MAX_DIGIT):
-    for elem in new_arr[digit]:
-      arr.append(elem)
-  
+    new_arr = [[] for _ in range(MAX_DIGIT)]
+    for elem in arr:
+        digit = (elem // p) % 10
+        new_arr[digit].append(elem)
+    
+    arr = []  
+    for digit in range(MAX_DIGIT):
+        for elem in new_arr[digit]:
+            arr.append(elem)
+    
     p *= 10
 
 for elem in arr:
-  print(elem, end=" ")
+    print(elem, end=" ")
